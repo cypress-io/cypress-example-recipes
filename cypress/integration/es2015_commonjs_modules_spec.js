1ef94d35783388926c3e884eade323cfd4ce8663
Brian Mann <brian@backbonerails.com>
2016-11-30 15:47:37 -0500

    nest module suites under a common parent suite

--- cypress/integration/es2015_commonjs_modules_spec.js
+++ cypress/integration/es2015_commonjs_modules_spec.js
@@ -20,38 +20,42 @@ import { upperFirst, lowerFirst } from 'lodash'
 // and also require node_modules from our package.json
 const minimist = require('minimist')
 
-// you can use ES2015 module import syntax
-context('ES2015', function(){
-  it('can import default function', function(){
-    expect(foo('bar')).to.eq('foo bar')
+describe("Modules", function(){
+
+  // you can use ES2015 module import syntax
+  context('ES2015', function(){
+    it('can import default function', function(){
+      expect(foo('bar')).to.eq('foo bar')
+    })
+
+    it('can import upperFirst from lodash', function(){
+      expect(upperFirst('brian')).to.eq('Brian')
+    })
+
+    it('can import lowerFirst from lodash', function(){
+      expect(lowerFirst('BRIAN')).to.eq('bRIAN')
+    })
   })
 
-  it('can import upperFirst from lodash', function(){
-    expect(upperFirst('brian')).to.eq('Brian')
-  })
+  // you can also use node's CommonJS require syntax
+  context('CommonJS', function(){
+    it('can require module.exports', function(){
+      expect(bar('baz')).to.eq('bar baz')
+    })
 
-  it('can import lowerFirst from lodash', function(){
-    expect(lowerFirst('BRIAN')).to.eq('bRIAN')
-  })
-})
+    it('can require minimist', function(){
+      // minimist is a command line argument parsing tool
+      // that is traditionally used in node (we use it at Cypress)
+      //
+      // but you can still require it and use it in browser tests too!
+      const args = ['--count=1', '--foo', 'bar', '--no-quux']
 
-context('Common JS', function(){
-  it('can require module.exports', function(){
-    expect(bar('baz')).to.eq('bar baz')
-  })
-
-  it('can require minimist', function(){
-    // minimist is a command line argument parsing tool
-    // that is traditionally used in node (we use it at Cypress)
-    //
-    // but you can still require it and use it in browser tests too!
-    const args = ['--count=1', '--foo', 'bar', '--no-quux']
-
-    expect(minimist(args)).to.deep.eq({
-      count: 1,
-      foo: 'bar',
-      quux: false,
-      _: []
+      expect(minimist(args)).to.deep.eq({
+        count: 1,
+        foo: 'bar',
+        quux: false,
+        _: []
+      })
     })
   })
 })
