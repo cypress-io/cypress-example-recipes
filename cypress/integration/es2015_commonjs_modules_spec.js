f7291eb6df470a86e3d2ae81f201dc569b35ff87
Brian Mann <brian@backbonerails.com>
2016-11-30 11:38:37 -0500

    double -> single quotes

--- cypress/integration/es2015_commonjs_modules_spec.js
+++ cypress/integration/es2015_commonjs_modules_spec.js
@@ -9,47 +9,47 @@
 // functions come from.
 
 // ES2015 import utility foo
-import foo from "../support/utils/foo"
+import foo from '../support/utils/foo'
 
 // CommonJS require utility bar
-const bar = require("../support/utils/bar")
+const bar = require('../support/utils/bar')
 
 // we can also import node_modules from our package.json
-import { upperFirst, lowerFirst } from "lodash"
+import { upperFirst, lowerFirst } from 'lodash'
 
 // and also require node_modules from our package.json
-const minimist = require("minimist")
+const minimist = require('minimist')
 
 // you can use ES2015 module import syntax
-context("ES2015", function(){
-  it("can import default function", function(){
-    expect(foo("bar")).to.eq("foo bar")
+context('ES2015', function(){
+  it('can import default function', function(){
+    expect(foo('bar')).to.eq('foo bar')
   })
 
-  it("can import upperFirst from lodash", function(){
-    expect(upperFirst("brian")).to.eq("Brian")
+  it('can import upperFirst from lodash', function(){
+    expect(upperFirst('brian')).to.eq('Brian')
   })
 
-  it("can import lowerFirst from lodash", function(){
-    expect(lowerFirst("BRIAN")).to.eq("bRIAN")
+  it('can import lowerFirst from lodash', function(){
+    expect(lowerFirst('BRIAN')).to.eq('bRIAN')
   })
 })
 
-context("Common JS", function(){
-  it("can require module.exports", function(){
-    expect(bar("baz")).to.eq("bar baz")
+context('Common JS', function(){
+  it('can require module.exports', function(){
+    expect(bar('baz')).to.eq('bar baz')
   })
 
-  it("can require minimist", function(){
+  it('can require minimist', function(){
     // minimist is a command line argument parsing tool
     // that is traditionally used in node (we use it at Cypress)
     //
     // but you can still require it and use it in browser tests too!
-    const args = ["--count=1", "--foo", "bar", "--no-quux"]
+    const args = ['--count=1', '--foo', 'bar', '--no-quux']
 
     expect(minimist(args)).to.deep.eq({
       count: 1,
-      foo: "bar",
+      foo: 'bar',
       quux: false,
       _: []
     })
