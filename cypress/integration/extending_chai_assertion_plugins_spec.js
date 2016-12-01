4feab90951154f9f3e8d246370b9a4558cd589a5
Brian Mann <brian@backbonerails.com>
2016-11-30 20:54:05 -0500

    double -> single quotes

--- cypress/integration/extending_chai_assertion_plugins_spec.js
+++ cypress/integration/extending_chai_assertion_plugins_spec.js
@@ -1,6 +1,6 @@
 // we installed this node_module in package.json
 // https://github.com/hurrymaplelad/chai-colors
-import chaiColors from "chai-colors"
+import chaiColors from 'chai-colors'
 
 // and we are extending chai to use this assertion
 // plugin but this plugin will only be available once
@@ -10,8 +10,8 @@ import chaiColors from "chai-colors"
 // it would not have access to this plugin
 chai.use(chaiColors)
 
-describe("Chai Assertion Plugins", function(){
-  context("chai-date-string", function(){
+describe('Chai Assertion Plugins', function(){
+  context('chai-date-string', function(){
     // we installed this node_module in package.json
     // https://github.com/googoid/chai-date-string
     //
@@ -23,38 +23,38 @@ describe("Chai Assertion Plugins", function(){
     // and you'll see that we import cypress/support/assertions.js
     // and because the supportFile is bundled before our spec file
     // it means we already have access to it
-    it("can be used in any spec file", function(){
-      expect("2015-11-12").to.be.a.dateString()
+    it('can be used in any spec file', function(){
+      expect('2015-11-12').to.be.a.dateString()
     })
 
-    it("can be wrapped by Cypress as well", function(){
-      cy.wrap("2016-03-14").should("be.a.dateString")
+    it('can be wrapped by Cypress as well', function(){
+      cy.wrap('2016-03-14').should('be.a.dateString')
     })
 
-    it("can be negated", function(){
-      expect("2015-14-41").not.to.be.a.dateString()
+    it('can be negated', function(){
+      expect('2015-14-41').not.to.be.a.dateString()
     })
 
-    it("can be negated by Cypress", function(){
-      cy.wrap("2015-14-41").should("not.be.a.dateString")
+    it('can be negated by Cypress', function(){
+      cy.wrap('2015-14-41').should('not.be.a.dateString')
     })
   })
 
-  context("chai-colors", function(){
-    it("can convert rgba to hex", function(){
-      expect("rgba(0, 0, 0, 1)").to.be.colored("#000000")
+  context('chai-colors', function(){
+    it('can convert rgba to hex', function(){
+      expect('rgba(0, 0, 0, 1)').to.be.colored('#000000')
     })
 
-    it("can be wrapped by Cypress as well", function(){
-      cy.wrap("rgba(0, 0, 0, 1)").should("be.colored", "#000000")
+    it('can be wrapped by Cypress as well', function(){
+      cy.wrap('rgba(0, 0, 0, 1)').should('be.colored', '#000000')
     })
 
-    it("can be negated", function(){
-      expect("#650042").not.to.be.colored("rgba(1, 2, 3, 4)")
+    it('can be negated', function(){
+      expect('#650042').not.to.be.colored('rgba(1, 2, 3, 4)')
     })
 
-    it("can be negated by Cypress", function(){
-      cy.wrap("#ff0000").should("not.be.colored", "green")
+    it('can be negated by Cypress', function(){
+      cy.wrap('#ff0000').should('not.be.colored', 'green')
     })
   })
 })
