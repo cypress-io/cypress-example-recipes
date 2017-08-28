2085733aa45f4a9f961f4a95158916d26224aa4d
Chris Breiding <chrisbreiding@gmail.com>
2017-08-28 16:00:47 -0400

    fix use of chai-enzyme

--- cypress/integration/unit_test_react_enzyme_spec.js
+++ cypress/integration/unit_test_react_enzyme_spec.js
@@ -4,6 +4,12 @@ import { shallow } from 'enzyme'
 
 import Greeting from '../../examples/unit_test_react_enzyme/greeting'
 
+// HACK: chai-enzyme does not play nice with chai-jquery, so remove the
+// problem-causing assertions that collide with chai-jquery
+'visible hidden selected checked enabled disabled'.split(' ').forEach((selector) => {
+  Object.defineProperty(chai.Assertion.prototype, selector, { get: () => {} })
+})
+
 // Cypress automatically exposes the 'chai'
 // global to all spec files. This enables us
 // to extend chai with enzyme specific assertions
@@ -12,7 +18,7 @@ import Greeting from '../../examples/unit_test_react_enzyme/greeting'
 // Alternatively we could move this configuration
 // into cypress/support/assertions to enable all
 // spec files to use these chai-enzyme assertions
-chai.use(chaiEnzyme)
+chai.use(chaiEnzyme())
 
 describe('Unit Test React with Enzyme', function(){
   context('<Greeting />', function(){
