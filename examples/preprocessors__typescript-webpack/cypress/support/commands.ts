0677f7a071e534eb5404ca5e801e721a35bb0e91
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-01-08 12:17:36 -0500

    show example of adding custom TS command, close #67

--- examples/preprocessors__typescript-webpack/cypress/support/commands.ts
+++ examples/preprocessors__typescript-webpack/cypress/support/commands.ts
@@ -23,3 +23,17 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// add a custom command cy.foo()
+Cypress.Commands.add('foo', () => 'foo')
+
+// see more example of adding custom commands to Cypress TS interface
+// in https://github.com/cypress-io/add-cypress-custom-command-in-typescript
+// add new command to the existing Cypress interface
+// tslint:disable-next-line no-namespace
+declare namespace Cypress {
+  // tslint:disable-next-line interface-name
+  interface Chainable {
+    foo: () => string
+  }
+}
