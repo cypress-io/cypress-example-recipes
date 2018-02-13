fcd749379c8f7d9099ef93d8ab191301fdc85c91
Brian Mann <brian@backbonerails.com>
2018-02-13 17:39:08 -0500

    enable logging in chrome

--- examples/stubbing-spying__google-analytics/cypress/plugins/index.js
+++ examples/stubbing-spying__google-analytics/cypress/plugins/index.js
@@ -14,4 +14,11 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  on('before:browser:launch', (browser, args) => {
+    if (browser.name === 'chrome') {
+      args.push('--enable-logging', '--v=1')
+
+      return args
+    }
+  })
 }
