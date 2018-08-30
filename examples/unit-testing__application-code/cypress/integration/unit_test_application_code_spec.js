e49ee68f3ab686c43717889fe19c906edfe7293c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-08-29 21:58:32 -0400

    add window variable example

--- examples/unit-testing__application-code/cypress/integration/unit_test_application_code_spec.js
+++ examples/unit-testing__application-code/cypress/integration/unit_test_application_code_spec.js
@@ -116,6 +116,14 @@ describe('Unit Test Application Code', function(){
       // until the subject {baz: 42} does NOT have property "baz"
       cy.wrap(o).its('foo.bar').should('not.have.property', 'baz')
     })
+
+    it('waits for a variable to be set on application\'s Window object', () => {
+      cy.visit('./index.html')
+      // similar to above examples, cy.window() yields application's Window
+      // object and we tell Cypress to retry until that object has property "AppReady"
+      // and its value is `true`.
+      cy.window().should('have.property', 'AppReady', true)
+    })
   })
 
   context('math.js', function(){
