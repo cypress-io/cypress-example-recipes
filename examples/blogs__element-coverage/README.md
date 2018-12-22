6385af8af70f3d3432254b4e04f187aeb3333d55
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-12-22 13:41:47 -0500

    working test

---
+++ examples/blogs__element-coverage/README.md
@@ -0,0 +1,10 @@
+# Element coverage
+
+Code for blog post [Element coverage](https://www.cypress.io/blog/2018/12/20/element-coverage/)
+
+## Shows how to
+
+- overwrite several built-in Cypress commands in [cypress/support/commands.js](cypress/support/commands.js) to keep track of elements the test interacts with
+- draw elements after the tests finish
+
+![Elements](img/elements.png)
---
+++ examples/blogs__element-coverage/cypress/integration/spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+/* eslint-env browser */
+beforeEach(() => {
+  cy.visit('/')
+})
+it('works', function () {
+  cy.get('.new-todo').type('first todo{enter}')
+  cy.get('.new-todo').type('second todo{enter}')
+  cy.get('.todo-list li').should('have.length', 2)
+    .first().find(':checkbox').check()
+
+  cy.contains('.filters a', 'Active').click()
+  cy.contains('.filters a', 'Active').should('have.class', 'selected')
+
+  cy.contains('.filters a', 'Completed').click()
+  cy.contains('.filters a', 'Completed').should('have.class', 'selected')
+
+  cy.contains('.filters a', 'All').click()
+  cy.contains('.filters a', 'All').should('have.class', 'selected')
+})
