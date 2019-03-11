4d0f75c4a7f6f764c7f5a17d4e8b1301759d0e36
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-11 12:04:00 -0400

    update chai plugin recipe

--- examples/extending-cypress__chai-assertions/README.md
+++ examples/extending-cypress__chai-assertions/README.md
@@ -1,5 +1,5 @@
 # Adding Chai Assertions
 
-- Extend [`chai`](http://chaijs.com/) with the [`chai-date-string`](http://chaijs.com/plugins/chai-date-string/) assertion plugin, see [cypress/support/assertions.js](cypress/support/assertions.js)
-- Extend [`chai`](http://chaijs.com/) with the [`chai-colors`](http://chaijs.com/plugins/chai-colors/) assertion plugin, see [spec file](cypress/integration/extending-chai-assertion-plugins-spec.js)
+- Extend [`chai`](http://chaijs.com/) with the [`chai-date-string`](http://chaijs.com/plugins/chai-date-string/) assertion plugin from [cypress/support/index.js](cypress/support/index.js) so it is available in every spec file.
+- Extend [`chai`](http://chaijs.com/) with the [`chai-colors`](http://chaijs.com/plugins/chai-colors/) assertion plugin in a single [spec file](cypress/integration/extending-chai-assertion-plugins-spec.js)
 - Globally extend [`chai`](http://chaijs.com/) for all specs.
--- examples/extending-cypress__chai-assertions/cypress/support/index.js
+++ examples/extending-cypress__chai-assertions/cypress/support/index.js
@@ -1,22 +1,9 @@
-// ***********************************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
+// because this file is imported from cypress/support/index.js
+// that means all other spec files will have this assertion plugin
+// available to them because the supportFile is bundled and served
+// prior to any spec files loading
+import chaiDateString from "chai-date-string"
 
-// Import commands.js using ES2015 syntax:
-import './commands'
-
-// also import 3rd party assertions
-// which will globally modify chai
-// and make these accessible to all specs
-import "./assertions"
+// chai is a global exposed by Cypress which means
+// we can just simply extend it
+chai.use(chaiDateString)
