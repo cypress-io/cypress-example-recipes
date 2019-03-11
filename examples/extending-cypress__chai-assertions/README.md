e9f6f319717697a23c1d6883950b7cbbe18475d4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-11 12:17:01 -0400

    add custom assertion example

--- examples/extending-cypress__chai-assertions/README.md
+++ examples/extending-cypress__chai-assertions/README.md
@@ -3,3 +3,4 @@
 - Extend [`chai`](http://chaijs.com/) with the [`chai-date-string`](http://chaijs.com/plugins/chai-date-string/) assertion plugin from [cypress/support/index.js](cypress/support/index.js) so it is available in every spec file.
 - Extend [`chai`](http://chaijs.com/) with the [`chai-colors`](http://chaijs.com/plugins/chai-colors/) assertion plugin in a single [spec file](cypress/integration/extending-chai-assertion-plugins-spec.js)
 - Globally extend [`chai`](http://chaijs.com/) for all specs.
+- Write and use own Chai assertion, see [cypress/support/index.js](cypress/support/index.js)
--- examples/extending-cypress__chai-assertions/cypress/integration/extending-chai-assertion-plugins-spec.js
+++ examples/extending-cypress__chai-assertions/cypress/integration/extending-chai-assertion-plugins-spec.js
@@ -1,3 +1,5 @@
+/// <reference types="Cypress" />
+
 // we installed this node_module in package.json
 // https://github.com/hurrymaplelad/chai-colors
 import chaiColors from 'chai-colors'
@@ -10,8 +12,8 @@ import chaiColors from 'chai-colors'
 // it would not have access to this plugin
 chai.use(chaiColors)
 
-describe('Chai Assertion Plugins', function(){
-  context('chai-date-string', function(){
+describe('Chai Assertion Plugins', function () {
+  context('chai-date-string', function () {
     // we installed this node_module in package.json
     // https://github.com/googoid/chai-date-string
     //
@@ -23,38 +25,48 @@ describe('Chai Assertion Plugins', function(){
     // you'll see that we import cypress/support/assertions.js
     // and because the supportFile is bundled before our spec file,
     // it means we already have access to it
-    it('can be used in any spec file', function(){
+    it('can be used in any spec file', function () {
       expect('2015-11-12').to.be.a.dateString()
     })
 
-    it('can be wrapped by Cypress as well', function(){
+    it('can be wrapped by Cypress as well', function () {
       cy.wrap('2016-03-14').should('be.a.dateString')
     })
 
-    it('can be negated', function(){
+    it('can be negated', function () {
       expect('2015-14-41').not.to.be.a.dateString()
     })
 
-    it('can be negated using Cypress', function(){
+    it('can be negated using Cypress', function () {
       cy.wrap('2015-14-41').should('not.be.a.dateString')
     })
   })
 
-  context('chai-colors', function(){
-    it('can convert rgba to hex', function(){
+  context('chai-colors', function () {
+    it('can convert rgba to hex', function () {
       expect('rgba(0, 0, 0, 1)').to.be.colored('#000000')
     })
 
-    it('can be wrapped by Cypress as well', function(){
+    it('can be wrapped by Cypress as well', function () {
       cy.wrap('rgba(0, 0, 0, 1)').should('be.colored', '#000000')
     })
 
-    it('can be negated', function(){
+    it('can be negated', function () {
       expect('#650042').not.to.be.colored('rgba(1, 2, 3, 4)')
     })
 
-    it('can be negated using Cypress', function(){
+    it('can be negated using Cypress', function () {
       cy.wrap('#ff0000').should('not.be.colored', 'green')
+    })
+  })
+
+  context('custom assertion', () => {
+    it('checks if a given string is "foo" or not', () => {
+      expect('foo').to.be.foo()
+      expect('bar').to.not.be.foo()
+
+      cy.wrap('foo').should('be.foo')
+      cy.wrap('bar').should('not.be.foo')
     })
   })
 })
