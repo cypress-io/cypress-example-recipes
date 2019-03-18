f4f84863751fa896ca3ca89a8ba798ffc21d7abd
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-18 10:54:39 -0400

    chore: update script

--- test-examples.js
+++ test-examples.js
@@ -6,13 +6,24 @@
 const globby = require('globby')
 const bluebird = require('bluebird')
 const tb = require('terminal-banner').terminalBanner
+const execa = require('execa')
+const pluralize = require('pluralize')
 
 const getExamples = () => {
   return globby('examples/*', { onlyFiles: false, expandDirectories: false })
 }
 
+const printFolders = (folders) => {
+  console.log(
+    'Will be running tests in %s',
+    pluralize('folder', folders.length, true)
+  )
+  folders.forEach((name) => console.log(' -', name))
+}
+
 const testExample = (folder) => {
   tb(`Testing ${folder}`)
+  return execa('npm', ['run', 'test:ci'], { stdio: 'inherit', cwd: folder })
 }
 
 const testExamples = (folders) => {
@@ -20,13 +31,15 @@ const testExamples = (folders) => {
 }
 
 const filterSomeFolders = (folders) => {
-  // blogs__codepen-demo breaks
+  // blogs__codepen-demo breaks in headless mode
   return folders.filter((folder) => !folder.includes('codepen'))
 }
 
-getExamples()
+bluebird
+.try(getExamples)
 .then((list) => list.slice(0, 3))
 .then(filterSomeFolders)
+.tap(printFolders)
 .then(testExamples)
 .catch((e) => {
   console.error(e.message)
