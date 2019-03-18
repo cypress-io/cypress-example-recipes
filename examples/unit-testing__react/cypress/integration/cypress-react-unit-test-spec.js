8aae21c0023745ef7d8679ec99c91ae183f5e953
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-18 17:04:52 -0400

    add cypress-react-unit-test

---
+++ examples/unit-testing__react/cypress/integration/cypress-react-unit-test-spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+// blocked by https://github.com/bahmutov/cypress-react-unit-test/issues/66
+// import 'cypress-react-unit-test'
+import React from 'react'
+import Greeting from '../../greeting'
+
+describe('cypress-react-unit-test', () => {
+  it('shows greeting', () => {
+    // cy.mount()
+  })
+})
--- examples/unit-testing__react/cypress/integration/react-testing-library-spec.js
+++ examples/unit-testing__react/cypress/integration/react-testing-library-spec.js
@@ -2,16 +2,18 @@ import React from 'react'
 import { render } from 'react-testing-library'
 import Greeting from '../../greeting'
 
-it('renders Greeting', () => {
-  const { getByText } = render(<Greeting />)
-  getByText('Hello World')
-})
+describe('react-testing-library', () => {
+  it('renders Greeting', () => {
+    const { getByText } = render(<Greeting />)
+    getByText('Hello World')
+  })
 
-it('changes greeting on click', () => {
-  const component = render(<Greeting />)
-  component
-    .getByLabelText('greeting')
-    .querySelector('button')
-    .click()
-  component.getByText('Bonjour World')
+  it('changes greeting on click', () => {
+    const component = render(<Greeting />)
+    component
+      .getByLabelText('greeting')
+      .querySelector('button')
+      .click()
+    component.getByText('Bonjour World')
+  })
 })
