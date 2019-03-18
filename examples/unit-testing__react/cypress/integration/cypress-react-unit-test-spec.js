402d09e8f477b7d95be46b6a1c92c1302d87da5a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-18 18:01:38 -0400

    working cypress-react-unit-test

--- examples/unit-testing__react/cypress/integration/cypress-react-unit-test-spec.js
+++ examples/unit-testing__react/cypress/integration/cypress-react-unit-test-spec.js
@@ -1,11 +1,17 @@
 /// <reference types="cypress" />
-// blocked by https://github.com/bahmutov/cypress-react-unit-test/issues/66
-// import 'cypress-react-unit-test'
+import 'cypress-react-unit-test'
 import React from 'react'
 import Greeting from '../../greeting'
 
 describe('cypress-react-unit-test', () => {
   it('shows greeting', () => {
-    // cy.mount()
+    cy.mount(<Greeting />)
+    cy.contains('Hello World')
+  })
+
+  it('changes greeting on click', () => {
+    cy.mount(<Greeting />)
+    cy.get('button').click()
+    cy.contains('Bonjour World')
   })
 })
