1138aa1b0442ad3cfc1d9587dfa2ded01a4fc8a6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-25 17:43:19 -0400

    add example with logging in once

--- examples/logging-in__single-sign-on/cypress/integration/logging-in-single-sign-on-spec.js
+++ examples/logging-in__single-sign-on/cypress/integration/logging-in-single-sign-on-spec.js
@@ -143,21 +143,28 @@ describe('Logging In - Single Sign on', function(){
         .should('contain', 'No session token set!')
     })
 
+
+    /**
+     * Assuming "cy.request" was called with `{followRedirect: false}` grabs the
+     * redirected to URI, parses it and returns just the "id_token".
+    */
+    const responseToToken = (resp) => {
+      // we can use the redirectedToUrl property that Cypress adds
+      // whenever we turn off following redirects
+      //
+      // and use node's url.parse module (and parse the query params)
+      const uri = url.parse(resp.redirectedToUrl, true)
+
+      // we now have query params as an object and can return
+      // the id_token
+      expect(uri.query).to.have.property('id_token')
+      return uri.query.id_token
+    }
+
     it('can parse out id_token and set on local storage', function(){
       // dont follow redirects so we can manually parse out the id_token
       cy.loginBySingleSignOn({followRedirect: false})
-        .then((resp) => {
-          // we can use the redirectedToUrl property that Cypress adds
-          // whenever we turn off following redirects
-          //
-          // and use node's url.parse module (and parse the query params)
-          const uri = url.parse(resp.redirectedToUrl, true)
-
-          // we now have query params as an object and can return
-          // the id_token
-          expect(uri.query).to.have.property('id_token')
-          return uri.query.id_token
-        })
+        .then(responseToToken)
         .then((id_token) => {
           cy.server()
           // observe the "GET /config" call from the application
@@ -196,6 +203,59 @@ describe('Logging In - Single Sign on', function(){
               })
             })
         })
+    })
+
+    describe('Log in once for speed', () => {
+      // in this example we follow SPA workflow, get the auth token once
+      // and then set it in window.localStorage before each test
+      // and voilá - we are logged in very quickly
+
+      before(function () {
+        // before any tests execute, get the token once
+        // as save it in the test context - thus the callback
+        // is using "function () { ... }" form and NOT arrow function
+        cy.loginBySingleSignOn({followRedirect: false})
+          .then(responseToToken)
+          .as('token') // saves under "this.token"
+      })
+
+      beforeEach(function () {
+        // before every test we need to grab "this.token"
+        // and set it in the local storage,
+        // so the application sends with and the user is authenticated
+        cy.on('window:before:load', win => {
+          win.localStorage.setItem('id_token', this.token)
+        })
+      })
+
+      it('opens page as logged in user', () => {
+        cy.visit('/')
+        cy.contains('"loggedIn":true')
+      })
+
+      it('config returns logged in: true', function () {
+        // note again this test uses "function () { ... }" callback
+        // in order to get access to the test context "this.token" saved above
+
+        cy.server()
+        // observe the "GET /config" call from the application
+        cy.route('/config').as('getConfig')
+
+        cy.visit('/')
+
+        cy.wait('@getConfig')
+          .then((xhr) => {
+            // inspect sent and received information
+            expect(xhr.request.headers, 'request includes token header')
+              .to.have.property('x-session-token', this.token)
+
+            expect(xhr.response.body, 'response body').to.deep.equal({
+              foo: 'bar',
+              loggedIn: true,
+              some: 'config'
+            })
+          })
+      })
     })
   })
 })
