d4eeb6aa63c2f22df2f3f365f3040d584b2575d1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-04-12 07:36:57 -0400

    run sorted by last modified

--- examples/fundamentals__npm-module/e2e-tests.js
+++ examples/fundamentals__npm-module/e2e-tests.js
@@ -1,13 +1,21 @@
-// runs Cypress end-to-end tests using Cypress NPM module APO
+// runs Cypress end-to-end tests using Cypress NPM module API
 // https://on.cypress.io/module-api
+
 const cypress = require('cypress')
 const globby = require('globby')
 const Promise = require('bluebird')
+const fs = require('fs')
+require('console.table')
 
-globby('./cypress/integration/*-spec.js').then(specs => {
-  console.log(specs)
-  return Promise.mapSeries(specs, runOneSpec)
-})
+const byTime = (a, b) => a.time - b.time
+
+const sortByLastModified = (filenames) => {
+  const withTimes = filenames.map(filename => ({
+    filename,
+    time: fs.statSync(filename).mtime.getTime()
+  }))
+  return withTimes.sort(byTime)
+}
 
 const runOneSpec = (spec) =>
   cypress
@@ -15,8 +23,13 @@ const runOneSpec = (spec) =>
       config: {
         video: false
       },
-      spec
+      spec: spec.filename
     })
 
-
+globby('./cypress/integration/*-spec.js')
+  .then(sortByLastModified)
+  .then(specs => {
+    console.table('Running specs in order', specs)
+    return Promise.mapSeries(specs, runOneSpec)
+  })
 
