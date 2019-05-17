45319da9e27107c4ebd998ca2bb0d4a67e076e52
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-05-17 11:38:56 -0400

    ignore uncaught exceptions from static site

--- examples/fundamentals__dynamic-tests/cypress/integration/spec.js
+++ examples/fundamentals__dynamic-tests/cypress/integration/spec.js
@@ -3,11 +3,16 @@ describe('Dynamic tests', () => {
   // logo selector on public static site after moving to Gatsby
   const logoSelector = 'img[alt="Cypress.io"]'
 
+  // ignore errors from the site itself
+  Cypress.on('uncaught:exception', () => {
+    return false
+  })
+
   context('viewports', () => {
     // run the same test against different viewport resolution
     const sizes = ['iphone-6', 'ipad-2', [1024, 768]]
 
-    sizes.forEach(size => {
+    sizes.forEach((size) => {
       it(`displays logo on ${size} screen`, () => {
         if (Cypress._.isArray(size)) {
           cy.viewport(size[0], size[1])
@@ -25,7 +30,7 @@ describe('Dynamic tests', () => {
     // run the same test against different subdomain
     const urls = ['https://docs.cypress.io', 'https://www.cypress.io']
 
-    urls.forEach(url => {
+    urls.forEach((url) => {
       it(`Should display logo on ${url}`, () => {
         cy.visit(url)
 
