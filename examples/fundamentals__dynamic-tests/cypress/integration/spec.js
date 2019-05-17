892cbab50916c0a5bb24c6becc74cf1d749a2702
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-05-17 09:50:24 -0400

    update test to account for different logo on static site

--- examples/fundamentals__dynamic-tests/cypress/integration/spec.js
+++ examples/fundamentals__dynamic-tests/cypress/integration/spec.js
@@ -1,10 +1,13 @@
 /// <reference types="cypress" />
 describe('Dynamic tests', () => {
+  // logo selector on public static site after moving to Gatsby
+  const logoSelector = 'img[alt="Cypress.io"]'
+
   context('viewports', () => {
     // run the same test against different viewport resolution
     const sizes = ['iphone-6', 'ipad-2', [1024, 768]]
 
-    sizes.forEach((size) => {
+    sizes.forEach(size => {
       it(`displays logo on ${size} screen`, () => {
         if (Cypress._.isArray(size)) {
           cy.viewport(size[0], size[1])
@@ -13,7 +16,7 @@ describe('Dynamic tests', () => {
         }
 
         cy.visit('https://www.cypress.io')
-        cy.get('#logo').should('be.visible')
+        cy.get(logoSelector).should('be.visible')
       })
     })
   })
@@ -22,10 +25,14 @@ describe('Dynamic tests', () => {
     // run the same test against different subdomain
     const urls = ['https://docs.cypress.io', 'https://www.cypress.io']
 
-    urls.forEach((url) => {
+    urls.forEach(url => {
       it(`Should display logo on ${url}`, () => {
         cy.visit(url)
-        cy.get('#logo').should('be.visible')
+
+        // our logo is different on static site vs documentation site
+        const selector = url.includes('www') ? logoSelector : '#logo'
+
+        cy.get(selector).should('be.visible')
       })
     })
   })
