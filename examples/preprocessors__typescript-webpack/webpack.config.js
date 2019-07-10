f012f3baafcfdca38fe6137313bcd6b089773960
Adam Misiorny <adam.misiorny@gmail.com>
2019-07-10 04:54:06 +0200

    Improve typescript example (#307)
    https://github.com/cypress-io/cypress/issues/2316

--- examples/preprocessors__typescript-webpack/webpack.config.js
+++ examples/preprocessors__typescript-webpack/webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
         exclude: [/node_modules/],
         use: [{
           loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
         }],
       },
     ],
