75a9451d9046f69cb29ec47d3a7e62a93b878cdd
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-07-22 15:07:03 -0400

    move code

--- examples/fundamentals__add-custom-command/cypress/integration/spec.js
+++ examples/fundamentals__add-custom-command/cypress/integration/spec.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 // enables intelligent code completion for Cypress commands
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="Cypress" />
@@ -9,14 +11,14 @@
  */
 Cypress.Commands.add('dataCy', (value) => cy.get(`[data-cy=${value}]`))
 
+it('finds element using data-cy custom command', () => {
+  cy.visit('index.html')
+  cy.dataCy('greeting').should('be.visible')
+})
+
 it('finds element using h1', () => {
   cy.visit('index.html')
   cy.get('h1')
   .should('be.visible')
   .and('have.attr', 'data-cy', 'greeting')
-})
-
-it('finds element using data-cy custom command', () => {
-  cy.visit('index.html')
-  cy.dataCy('greeting').should('be.visible')
 })
