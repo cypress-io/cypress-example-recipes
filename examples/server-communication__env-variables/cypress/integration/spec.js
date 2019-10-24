ae0130385a9d086c4a91306cd716576573b221f4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-10-24 09:42:06 -0400

    add failing env test

--- examples/server-communication__env-variables/cypress/integration/spec.js
+++ examples/server-communication__env-variables/cypress/integration/spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 describe('process environment variables', () => {
   it('has variable my-var from cypress.json', () => {
-    expect(Cypress.env('my-var')).to.equal("ok")
+    expect(Cypress.env('my-var')).to.equal('ok')
   })
 
   it('has variables FOO and BAR from process.env', () => {
@@ -9,7 +9,7 @@ describe('process environment variables', () => {
     // see how FOO and BAR were copied in "cypress/plugins/index.js"
     expect(Cypress.env()).to.contain({
       FOO: '42',
-      BAR: 'baz'
+      BAR: 'baz',
     })
   })
 
@@ -17,5 +17,9 @@ describe('process environment variables', () => {
     // CYPRESS_ping=123 cypress open
     // NOTE passed variable is a number
     expect(Cypress.env('ping')).to.equal(123)
+  })
+
+  it('has unicode value', () => {
+    expect(Cypress.env('unicode-var')).to.equal('привет')
   })
 })
