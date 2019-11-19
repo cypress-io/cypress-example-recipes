fbf3b438d877c035ef704f71820e0eaeafb118c6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-11-19 10:17:55 -0500

    add custom Brave browser example

---
+++ examples/fundamentals__custom-browsers/README.md
@@ -0,0 +1,30 @@
+# Custom browsers
+
+Cypress v3.7.0+ allows the project to customize the list of browsers. In this example, [cypress/plugins/index.js](cypress/plugins/index.js) filters the list of all browsers detected on the user's machine and only returns the bundled Electron browser. Or you could do the opposite and only allow the project to use Chrome browsers
+
+```js
+// cypress/plugins/index.js
+module.exports = (on, config) => {
+  return {
+    browsers: config.browsers.filter((browser) => browser.family === 'chrome'),
+  }
+}
+```
+
+![Only Chrome browsers displayed](images/only-chrome.png)
+
+## Custom browser
+
+Plugins file can also adjust or overwrite the list of browsers. For example [cypress/plugins/brave.js](cypress/plugins/brave.js) file finds the Brave Browser and returns only that browser.
+
+```shell
+npx cypress open --config pluginsFile=cypress/plugins/brave.js
+```
+
+![Brave browser running tests](images/brave.png)
+
+If you want to use Brave browser during non-interactive run, pass its name
+
+```shell
+npx cypress open --config pluginsFile=cypress/plugins/brave.js --browser brave
+```
---
+++ examples/fundamentals__custom-browsers/cypress/plugins/brave.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-console */
+const os = require('os')
+const execa = require('execa')
+const tasks = require('./tasks')
+
+/**
+ * Parses STDOUT from "brave-browser --version" command.
+ * @param {string} stdout from Brave, something like "Brave Browser 77.0.69.135"
+ */
+const parseBraveVersion = (stdout) => {
+  const [, version] = /Brave Browser (\d+\.\d+\.\d+\.\d+)/.exec(stdout)
+
+  console.log('Brave browser version is', version)
+  const majorVersion = parseFloat(version.split('.')[0])
+
+  return { version, majorVersion }
+}
+
+const findBraveBrowserInfo = (browserPath) => {
+  return execa(browserPath, ['--version'])
+  .then((result) => result.stdout)
+  .then(parseBraveVersion)
+  .then(({ version, majorVersion }) => {
+    return {
+      name: 'brave',
+      family: 'chrome',
+      displayName: 'Brave',
+      version,
+      path: browserPath,
+      majorVersion,
+    }
+  })
+}
+
+const findBraveBrowser = () => {
+  // the path is hard-coded for simplicity
+  switch (os.platform()) {
+    case 'darwin':
+      return findBraveBrowserInfo(
+        '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
+      )
+    case 'linux':
+      return findBraveBrowserInfo('brave-browser')
+    default:
+      throw new Error(`Cannot find Brave browser on platform ${os.platform()}`)
+  }
+}
+
+module.exports = (on, config) => {
+  on('task', tasks)
+
+  // only filter browsers if we are passed a list - this way
+  // this example works on Cypress v3.7.0+ and below
+  if (!config.browsers) {
+    return
+  }
+
+  // Cypress v3.7.0+
+  return findBraveBrowser().then((browser) => {
+    return {
+      browsers: [browser],
+    }
+  })
+}
