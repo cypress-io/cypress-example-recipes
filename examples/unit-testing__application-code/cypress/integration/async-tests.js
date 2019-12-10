53f0aad91ceee460d235f4ede0ed482a8acd1020
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-12-10 12:39:11 -0500

    more async examples

--- examples/unit-testing__application-code/cypress/integration/async-tests.js
+++ examples/unit-testing__application-code/cypress/integration/async-tests.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-import {reverseString} from '../../async-methods'
+import {reverseString, twice} from '../../async-methods'
 
 describe('Async reverse', () => {
   it('reverses a string', () => {
@@ -18,5 +18,34 @@ describe('Async reverse', () => {
     // to deal with promises
     const result = await reverseString('bar')
     expect(result).to.equal('rab')
+  })
+
+  it('reverses text loaded from a fixture file', () => {
+    // when you use Cypress commands like "cy.fixture" the test
+    // becomes asynchronous automatically. Cypress waits for all
+    // commands to finish, so you don't need to return a promise
+    // from the test itself
+    cy.fixture('string.txt', 'utf8')
+      .then(Cypress._.trim)
+      // if you call your Promise-returning functions
+      // cy.then automatically waits for the promise to resolve
+      // https://on.cypress.io/then
+      .then(reverseString)
+      .should('equal', 'xof nworb')
+  })
+})
+
+describe('Async twice', () => {
+  it('doubles numbers', () => {
+    return twice(2).then(result => expect(result).to.equal(4))
+  })
+
+  it('repeats a string', () => {
+    // if we start using Cypress commands, the test becomes asynchronous
+    // and will wait for all commands in the chain to finish
+    // before the test finishes
+    cy.wrap('fox')
+      .then(twice).then(twice)
+      .should('equal', 'foxfoxfoxfox')
   })
 })
