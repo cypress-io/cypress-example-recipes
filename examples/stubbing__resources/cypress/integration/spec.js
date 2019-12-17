0dd3d1173e2a3fe5a0d98e29c4c31e6df2c1d31f
Josef Biehler <gabbersepp@googlemail.com>
2019-12-17 06:48:15 +0100

    load picture from fixtures remove ajax call to make the example not more complexe than needed

--- examples/stubbing__resources/cypress/integration/spec.js
+++ examples/stubbing__resources/cypress/integration/spec.js
@@ -3,27 +3,15 @@ var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 describe('Stub loading of resources', () => {
   it('requested image can be exchanged', () => {
-    cy.server();
-    cy.route({
-      method: 'GET',
-      url: '/apirequest',
-      response: JSON.stringify({
-        src: "https://cypress.io/cypress.png"
-      })
-    });
-
     cy.visit("index.html").then(() => {
-      cy.get("#img-container").then($element => {
+      let imgStr;
+
+      cy.fixture("picture.png", "base64").then(imgString => {
+        imgStr = imgString;
+      }).get("#img-container").then($element => {
         var el = $element[0];
         observeDOM(el, mutations => {
-          // red dot
-          mutations[0].addedNodes[0].src = "data:image/png;base64," +
-            "iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAABGdBTUEAALGP" +
-            "C/xhBQAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB9YGARc5KB0XV+IA" +
-            "AAAddEVYdENvbW1lbnQAQ3JlYXRlZCB3aXRoIFRoZSBHSU1Q72QlbgAAAF1J" +
-            "REFUGNO9zL0NglAAxPEfdLTs4BZM4DIO4C7OwQg2JoQ9LE1exdlYvBBeZ7jq" +
-            "ch9//q1uH4TLzw4d6+ErXMMcXuHWxId3KOETnnXXV6MJpcq2MLaI97CER3N0" +
-            "vr4MkhoXe0rZigAAAABJRU5ErkJggg=="
+          mutations[0].addedNodes[0].src = "data:image/png;base64," + imgStr;
         })
       })
       cy.get("button").click()
