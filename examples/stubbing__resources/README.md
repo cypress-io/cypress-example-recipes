38d5e8b28e58302d4927cb896cf2164735c0a6c1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-12-18 16:07:43 -0500

    added comments for clarity

--- examples/stubbing__resources/README.md
+++ examples/stubbing__resources/README.md
@@ -1,7 +1,7 @@
 # Stubbing Resources
 
-The spec file [cypress/integration/spec.js](cypress/integration/spec.js) shows how to stub resources that are loaded e.g. by `img` tag.
+The spec file [cypress/integration/spec.js](cypress/integration/spec.js) shows how to stub resources that are loaded e.g. by `img` tag. This works by observing DOM mutations and changing the `src` attribute.
 
-When clicking onto the button in the `index.html`, an api call is simulated that would provide an image url. This image is manipulated in order to display a predefined image.
+When clicking onto the button in the `index.html`, a new `img` tag is created that is supposed to load [images/rose.png](images/rose.png). Instead we load a red image from the fixture file [cypress/fixtures/picture.png](cypress/fixtures/picture.png).
 
-![Test example](images/resources-example.png)
+![Test example](images/example.png)
--- examples/stubbing__resources/cypress/integration/spec.js
+++ examples/stubbing__resources/cypress/integration/spec.js
@@ -1,33 +1,48 @@
 /// <reference types="cypress" />
-var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+/* global window */
+const MutationObserver = window.MutationObserver || window.WebKitMutationObserver
+
+// helper function to observe DOM changes
+function observeDOM (obj, callback) {
+  if (!obj || obj.nodeType !== window.Node.ELEMENT_NODE) {
+    throw new Error('can not observe this element')
+  }
+
+  const obs = new MutationObserver(callback)
+
+  obs.observe(obj, { childList: true, subtree: true })
+}
 
 describe('Stub loading of resources', () => {
   it('requested image can be exchanged', () => {
-    cy.visit("index.html").then(() => {
-      let imgStr;
+    cy.visit('index.html')
 
-      cy.fixture("picture.png", "base64").then(imgString => {
-        imgStr = imgString;
-      }).get("#img-container").then($element => {
-        var el = $element[0];
-        observeDOM(el, mutations => {
-          mutations[0].addedNodes[0].src = "data:image/png;base64," + imgStr;
-        })
-      })
-      cy.get("button").click()
-      cy.get("#img-container > img").should($img => expect($img[0].naturalWidth).to.not.equal(0))
+    let imgStr
+
+    // load mock image data from the fixture file
+    // and save in a local variable
+    cy.fixture('picture.png', 'base64')
+    .then((imgString) => {
+      imgStr = imgString
     })
-  });
-});
 
-// helper function to observe DOM changes
-function observeDOM(obj, callback) {
-  if (!obj || obj.nodeType !== Node.ELEMENT_NODE) {
-    throw new Error("can not observe this element");
-  }
+    // start watching the image container for new elements added
+    cy.get('#img-container').then(($element) => {
+      let el = $element[0]
 
-  var obs = new MutationObserver(mutations => {
-    callback(mutations);
-  });
-  obs.observe(obj, { childList: true, subtree: true });
-}
+      observeDOM(el, (mutations) => {
+        // we could check the type of the added node
+        // but for now we are assuming it is a new image node
+        mutations[0].addedNodes[0].src = `data:image/png;base64,${imgStr}`
+      })
+    })
+    cy.get('button').click()
+
+    // image element will have "naturalWidth" set when it loads
+    // the page tries to load "images/rose.png" with dimensions 70x46
+    // but the test will substitute "cypress/fixtures/picture.png" that is 100x100
+    cy.get('#img-container > img').should(($img) => expect($img[0].naturalWidth).to.equal(100))
+  })
+})
+
+
