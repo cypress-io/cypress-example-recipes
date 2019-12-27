c140fef5b0488af015f39cadef8e5a09ef5b8dd9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-12-27 12:52:01 -0500

    add test ci scripts

--- examples/blogs__use-react-devtools/cypress/plugins/index.js
+++ examples/blogs__use-react-devtools/cypress/plugins/index.js
@@ -4,8 +4,11 @@ const path = require('path')
 
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  on('before:browser:launch', (browser = {}, args) => {
-    if (browser.family === 'chrome') {
+  on('before:browser:launch', (browser, args) => {
+    console.log('launching browser %o', browser)
+
+    // only load React DevTools extension when opening Chrome in interactive mode
+    if (browser.family === 'chrome' && browser.isHeaded) {
       const extensionFolder = path.resolve(__dirname, '..', '..', '4.2.1_0')
 
       console.log('adding React DevTools extension from', extensionFolder)
