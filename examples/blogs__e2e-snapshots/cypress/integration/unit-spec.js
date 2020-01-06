06ff1e4ea5ed93c82b5868927aa31ddb60a58655
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-01-06 13:26:54 -0500

    fix many eslint errors

--- examples/blogs__e2e-snapshots/cypress/integration/unit-spec.js
+++ examples/blogs__e2e-snapshots/cypress/integration/unit-spec.js
@@ -1,17 +1,18 @@
 /// <reference types="cypress" />
-/* eslint-env mocha */
-/* global cy */
 const add = (a, b) => a + b
-it('adds numbers', () => {
-  cy.wrap(add(2, 3)).snapshot()
-  cy.wrap(add(1, 10)).snapshot()
-  cy.wrap(add(-6, -3)).snapshot({ name: 'negatives' })
-})
 
-it('converts string to lowercase', function () {
-  cy.wrap('My STRING')
-  .invoke('toLowerCase')
-  .snapshot({
-    name: 'lowercase string',
+describe('functions', () => {
+  it('adds numbers', () => {
+    cy.wrap(add(2, 3)).snapshot()
+    cy.wrap(add(1, 10)).snapshot()
+    cy.wrap(add(-6, -3)).snapshot({ name: 'negatives' })
+  })
+
+  it('converts string to lowercase', function () {
+    cy.wrap('My STRING')
+    .invoke('toLowerCase')
+    .snapshot({
+      name: 'lowercase string',
+    })
   })
 })
