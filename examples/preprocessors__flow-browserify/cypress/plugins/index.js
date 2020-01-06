ac5959dd847b028ba2f937bc128b6093e8dae9f8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-01-06 13:47:28 -0500

    more linting

--- examples/preprocessors__flow-browserify/cypress/plugins/index.js
+++ examples/preprocessors__flow-browserify/cypress/plugins/index.js
@@ -1,7 +1,8 @@
 const browserify = require('@cypress/browserify-preprocessor')
 
 module.exports = (on, config) => {
-	const options = browserify.defaultOptions
-	options.browserifyOptions.transform[1][1].presets.push('@babel/preset-flow')
-	on('file:preprocessor', browserify(options))
+  const options = browserify.defaultOptions
+
+  options.browserifyOptions.transform[1][1].presets.push('@babel/preset-flow')
+  on('file:preprocessor', browserify(options))
 }
