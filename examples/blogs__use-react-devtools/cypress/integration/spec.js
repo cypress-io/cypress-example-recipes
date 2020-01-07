f3da778947d70d621ed82880c8882a0e950dced8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-01-07 08:52:00 -0500

    lint react dev tools recipe

--- examples/blogs__use-react-devtools/cypress/integration/spec.js
+++ examples/blogs__use-react-devtools/cypress/integration/spec.js
@@ -1,7 +1,9 @@
 /// <reference types="cypress" />
-it('loads React DevTools extension', () => {
-  cy.visit('localhost:3000')
-  // ? how do we confirm this is working?
-  cy.get('.board-row').eq(0).find('.square').eq(0).click()
-  cy.get('.board-row').eq(0).find('.square').eq(1).click()
+describe('React DevTools', () => {
+  it('loads React DevTools extension', () => {
+    cy.visit('localhost:3000')
+    // ? how do we confirm this is working?
+    cy.get('.board-row').eq(0).find('.square').eq(0).click()
+    cy.get('.board-row').eq(0).find('.square').eq(1).click()
+  })
 })
