448847a15a41d8f7807212980f54bc69c2be5a8b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-01-08 09:55:43 -0500

    more tests

--- examples/blogs__assertion-counting/cypress/integration/spec.js
+++ examples/blogs__assertion-counting/cypress/integration/spec.js
@@ -1,11 +1,66 @@
 /// <reference types="cypress" />
+import 'cypress-wait-until'
+
 describe('Window confirm', () => {
-  it('calls window confirm', () => {
+  // NOTE: skipping this test because it does not wait correctly for assertion to execute
+  it.skip('calls window confirm', () => {
     cy.visit('index.html')
     cy.on('window:confirm', (message) => {
       expect(message).to.equal('Are you sure?')
     })
 
     cy.get('#click').click()
+  })
+
+  it('waits for window confirm to happen using stub', () => {
+    cy.visit('index.html')
+    cy.on('window:confirm', cy.stub().as('confirm'))
+
+    cy.get('#click').click()
+    // test automatically waits for the stub
+    cy.get('@confirm').should('have.been.calledWith', 'Are you sure?')
+  })
+
+  it('waits for window confirm to happen using variable', () => {
+    cy.visit('index.html')
+    let called
+
+    cy.on('window:confirm', (message) => {
+      expect(message).to.equal('Are you sure?')
+      called = true
+    })
+
+    cy.get('#click').click()
+    // test automatically waits for the variable "called"
+    cy.wrap(null).should(() => {
+      expect(called).to.be.true
+    })
+  })
+
+  it('waits for window confirm to happen using promises', () => {
+    cy.visit('index.html')
+    let calledPromise = new Promise((resolve) => {
+      cy.on('window:confirm', (message) => {
+        expect(message).to.equal('Are you sure?')
+        resolve()
+      })
+    })
+
+    // test automatically waits for the promise
+    cy.get('#click').click().then(() => calledPromise)
+  })
+
+  it('waits for window confirm to happen using cy.waitUntil', () => {
+    cy.visit('index.html')
+    let called
+
+    cy.on('window:confirm', (message) => {
+      expect(message).to.equal('Are you sure?')
+      called = true
+    })
+
+    cy.get('#click').click()
+    // see https://github.com/NoriSte/cypress-wait-until
+    cy.waitUntil(() => called)
   })
 })
