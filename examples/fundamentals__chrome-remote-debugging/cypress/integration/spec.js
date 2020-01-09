12f71543780ffd79956ca9392f80799c794a7646
Josef Biehler <gabbersepp@googlemail.com>
2020-01-09 22:34:13 +0100

    add chrome-remote-interface example with native events
    * initial commit
    
    * add working example
    
    * adjust circle.yml
    
    * move dependency to root project
    
    * add readme, link to issues regarding "headless" flag, execute job only with chrome browser
    temporary execute job in my branch
    
    * adjust readme
    
    * reset CRI
    
    * reuse client if possible
    
    Co-authored-by: Gleb Bahmutov <gleb.bahmutov@gmail.com>

---
+++ examples/fundamentals__chrome-remote-debugging/cypress/integration/spec.js
@@ -0,0 +1,32 @@
+describe("Test", () => {
+    beforeEach(() => {
+        cy.task("resetCRI").visit("index.html");
+    })
+    it("Printable div is not visible normally", () => {
+        cy.get("#printable").should("not.be.visible");
+    })
+    it("Printable div is visible with 'print' media query", () => {
+        cy.task("activatePrintMediaQuery");
+        cy.get("#printable").should("be.visible");
+    })
+    it("Hover pseudo element is inactive normally", () => {
+        cy.get("#pseudo-hover span").should("not.be.visible");
+    })
+    it("Hover pseudo element is active", () => {
+        cy.task("activateHoverPseudo", { selector: "#pseudo-hover" });
+        cy.get("#pseudo-hover span").should("be.visible");
+    })
+
+    describe("reset to clean state", () => {
+        it("check visibility of div", () => {
+            cy.get("#not-in-printable").should("be.visible");
+            cy.task("activatePrintMediaQuery");
+            cy.get("#not-in-printable").should("not.be.visible");
+        })
+        it("div should be visible again before print media query is activated", () => {
+            cy.get("#not-in-printable").should("be.visible");
+            cy.task("activatePrintMediaQuery");
+            cy.get("#not-in-printable").should("not.be.visible");
+        })
+    })
+})
---
+++ examples/fundamentals__chrome-remote-debugging/cypress/plugins/index.js
@@ -0,0 +1,58 @@
+const CDP = require('chrome-remote-interface');
+
+function ensureRdpPort(args) {
+    const existing = args.find(arg => arg.slice(0, 23) === '--remote-debugging-port')
+
+    if (existing) {
+        return Number(existing.split('=')[1])
+    }
+
+    const port = 40000 + Math.round(Math.random() * 25000)
+    args.push(`--remote-debugging-port=${port}`)
+    return port
+}
+
+let port = 0;
+let client = null;
+
+module.exports = (on, config) => {
+    on('before:browser:launch', (browser, args) => {
+        port = ensureRdpPort(args);
+        // https://github.com/cypress-io/cypress/issues/832
+        // https://github.com/cypress-io/cypress/issues/5949
+        // remove comments above after the issues have ben fixed
+        args.push('--headless');
+    })
+    on("task", {
+        resetCRI: async () => {
+            if (client) {
+                await client.close();
+                client = null;
+            }
+
+            return Promise.resolve(true);
+        },
+        activatePrintMediaQuery: async () => {
+            client = client || await CDP({ port });
+            return client.send('Emulation.setEmulatedMedia', { media: "print" })
+        },
+        activateHoverPseudo: async ({ selector }) => {
+            client = client || await CDP({ port });
+            await client.DOM.enable();
+            await client.CSS.enable();
+            // as the Window consists of two IFrames, we must retrive the right one
+            const allRootNodes = await client.DOM.getFlattenedDocument();
+            const filtered = allRootNodes.nodes.filter(node => node.nodeName == "IFRAME" && node.contentDocument);
+            // The first IFrame is our App
+            const root = filtered[0].contentDocument;
+            const { nodeId } = await client.DOM.querySelector({
+                nodeId: root.nodeId,
+                selector
+            });
+            return client.CSS.forcePseudoState({
+                nodeId,
+                forcedPseudoClasses: ['hover']
+            });
+        }
+    })
+}
