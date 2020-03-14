1812d3e9eaf3f39104cafaa1f7722558e34f7658
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-03-14 13:46:19 -0400

    add removing items from select2

--- examples/testing-dom__select2/cypress/integration/spec.js
+++ examples/testing-dom__select2/cypress/integration/spec.js
@@ -59,6 +59,8 @@ describe('select2', () => {
 
     it('selects Massachusetts by typing', () => {
       cy.log('--- Pick state by typing ---')
+      // Select2 widget is inserted immediately after our <select> node
+      // thus we can click on it using CSS " + " selector
       cy.get('#favorite-state + .select2').click()
       cy.get('input[aria-controls="select2-favorite-state-results"]').type('Mass{enter}')
 
@@ -97,6 +99,43 @@ describe('select2', () => {
       // .should('have.length', 3)
       // .then((list) => list.map((k, el) => el.title))
       // .should('deep.equal', ['Connecticut', 'Massachusetts', 'Vermont'])
+    })
+
+    it('adds several states by typing', () => {
+      // Select2 HTML widget is inserted after the corresponding <select> element
+      // thus we can find it using " + " CSS selector
+      cy.get('#states + .select2').click()
+      // after we click on the Select2 widget, the search drop down and input appear
+      .find('.select2-search')
+      // type parts of the states' names, just like a real user would
+      .type('Verm{enter}Mass{enter}Conn{enter}')
+
+      // confirm the selected value - note that the values are sorted
+      // and because it is an array we need to use deep equality
+      // against the yielded list from ".invoke('val')"
+      cy.get('#states').invoke('val').should('deep.equal', ['CT', 'MA', 'VT'])
+    })
+
+    it('removes state', () => {
+      cy.get('#states + .select2').click()
+      .find('.select2-search')
+      .type('Verm{enter}Mass{enter}Conn{enter}')
+
+      // confirm 3 states are selected
+      cy.get('#states').invoke('val').should('deep.equal', ['CT', 'MA', 'VT'])
+
+      // remove Connecticut, they don't have a major league team anyway
+      cy.get('#states + .select2')
+      .contains('.select2-selection__choice', 'Connecticut')
+      .find('.select2-selection__choice__remove').click()
+
+      // when removing an item, Select2 automatically expands choices
+      // we can close them by pressing "Enter"
+      cy.get('#states + .select2 .select2-search')
+      .type('{enter}')
+
+      // confirm the remaining selections
+      cy.get('#states').invoke('val').should('deep.equal', ['MA', 'VT'])
     })
   })
 })
