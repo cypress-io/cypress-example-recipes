dab29c67ae15d85feb8305216817373920fac76b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-03-14 12:55:14 -0400

    more examples

--- examples/testing-dom__select2/cypress/integration/spec.js
+++ examples/testing-dom__select2/cypress/integration/spec.js
@@ -1,31 +1,102 @@
 /// <reference types="cypress" />
-/* eslint-disable mocha/no-global-tests */
-describe('select2', () => {
-  it('selects Massachusetts', () => {
-    cy.visit('index.html')
+beforeEach(() => {
+  cy.visit('index.html')
+})
 
-    cy.log('--- Force select ---')
-    // https://on.cypress.io/select
-    cy.get('#favorite-state').select('MA', { force: true })
+describe('HTML select element', () => {
+  context('single value', () => {
+    it('sets MA', () => {
+      // https://on.cypress.io/select
+      // set using value
+      // <option value="MA">Massachusetts</option>
+      cy.get('#my-state').select('MA')
 
-    // confirm the value of the selected element
-    cy.get('#favorite-state').should('have.value', 'MA')
+      // confirm the selected value
+      cy.get('#my-state').should('have.value', 'MA')
+    })
 
-    // confirm Select2 widget renders the state name
-    cy.get('#select2-favorite-state-container').should('have.text', 'Massachusetts')
+    it('sets Massachusetts', () => {
+      // https://on.cypress.io/select
+      // set using text
+      // <option value="MA">Massachusetts</option>
+      cy.get('#my-state').select('Massachusetts')
+
+      // confirm the selected value
+      cy.get('#my-state').should('have.value', 'MA')
+    })
   })
 
-  it('selects Massachusetts by typing', () => {
-    cy.visit('index.html')
+  context('multiple values', () => {
+    it('adds several states', () => {
+      // https://on.cypress.io/select
+      // select multiple values by passing an array
+      cy.get('#my-states').select(['MA', 'VT', 'CT'])
 
-    cy.log('--- Pick state by typing ---')
-    cy.get('#favorite-state + .select2').click()
-    cy.get('input[aria-controls="select2-favorite-state-results"]').type('Mass{enter}')
+      // confirm the selected value - note that the values are sorted
+      // and because it is an array we need to use deep equality
+      // against the yielded list from ".invoke('val')"
+      cy.get('#my-states').invoke('val').should('deep.equal', ['CT', 'MA', 'VT'])
+    })
+  })
+})
 
-    // confirm the value of the selected element
-    cy.get('#favorite-state').should('have.value', 'MA')
+describe('select2', () => {
+  context('single value', () => {
+    it('selects Massachusetts', () => {
+      cy.log('--- Force select ---')
 
-    // confirm Select2 widget renders the state name
-    cy.get('#select2-favorite-state-container').should('have.text', 'Massachusetts')
+      // https://on.cypress.io/select
+      // avoid error message that select element is covered
+      // by the rendered Select2 widget by using "force: true" option
+      cy.get('#favorite-state').select('MA', { force: true })
+
+      // confirm the value of the selected element
+      cy.get('#favorite-state').should('have.value', 'MA')
+
+      // confirm Select2 widget renders the state name
+      cy.get('#select2-favorite-state-container').should('have.text', 'Massachusetts')
+    })
+
+    it('selects Massachusetts by typing', () => {
+      cy.log('--- Pick state by typing ---')
+      cy.get('#favorite-state + .select2').click()
+      cy.get('input[aria-controls="select2-favorite-state-results"]').type('Mass{enter}')
+
+      // confirm the value of the selected element
+      cy.get('#favorite-state').should('have.value', 'MA')
+
+      // confirm Select2 widget renders the state name
+      cy.get('#select2-favorite-state-container').should('have.text', 'Massachusetts')
+    })
+  })
+
+  context('multiple values', () => {
+    it('adds several states', () => {
+      // https://on.cypress.io/select
+      // select multiple values by passing an array
+      // again, have to use "force": "true" because the actual
+      // select is covered by Select2 widget
+      cy.get('#states').select(['MA', 'VT', 'CT'], { force: true })
+
+      // confirm the selected value - note that the values are sorted
+      // and because it is an array we need to use deep equality
+      // against the yielded list from ".invoke('val')"
+      cy.get('#states').invoke('val').should('deep.equal', ['CT', 'MA', 'VT'])
+
+      // confirm Select2 widget renders the 3 state names
+      cy.get('#states + .select2 .select2-selection__choice')
+      // use ".should(cb)" to confirm the displayed values
+      // https://on.cypress.io/should#Function
+      .should((list) => {
+        expect(list[0].title).to.equal('Connecticut')
+        expect(list[1].title).to.equal('Massachusetts')
+        expect(list[2].title).to.equal('Vermont')
+      })
+
+      // TODO find a shorter way to express the assertion?
+      // .should('have.length', 3)
+      // .then((list) => list.map((k, el) => el.title))
+      // .should('deep.equal', ['Connecticut', 'Massachusetts', 'Vermont'])
+    })
   })
 })
