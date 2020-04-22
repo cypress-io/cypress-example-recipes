b7f9133588748d0fe50d5eea8f55e39f198af29c
Jennifer Shehane <jennifer@cypress.io>
2020-04-22 11:17:10 +0630

    minor updates

--- examples/testing-dom__csv-table/README.md
+++ examples/testing-dom__csv-table/README.md
@@ -1,4 +1,5 @@
 # CSV and table recipe
+
 > Loads CSV file and checks HTML table to have the text content matching the loaded CSV records
 
 Uses [neat-csv](https://github.com/sindresorhus/neat-csv) to parse the loaded CSV text into records.
@@ -11,16 +12,17 @@ Uses [neat-csv](https://github.com/sindresorhus/neat-csv) to parse the loaded CS
 
 ## Speed
 
-The spec file has 4 tests. The slowest wraps _every_ access to the table's row and cell in [Cypress alias variable](https://on.cypress.io/variables-and-aliases). While convenient, unless the cell's values are changing and need to [retry assertions](https://on.cypress.io/retry-ability), it introduces performance overhead.
+The spec file has 4 tests. The slowest wraps _every_ access to the table's row and cell in a [Cypress alias variable](https://on.cypress.io/variables-and-aliases). While convenient, unless the cell's values are changing and need to [retry assertions](https://on.cypress.io/retry-ability), it introduces performance overhead.
 
 The spec includes a few additional tests that avoid aliases and run much faster:
-- access every row then use DOM to access and check each child cell
+
+- access every row then use the DOM to access and check each child cell
 - access every row, extract `innerText` from every cell and compare the entire list at once
 - access the entire table, extract `innerText` from every cell and compare the entire list of values at once
 
 ![All tests](images/all-tests.png)
 
-We can get the test duration by subscribing to an event
+We can get the test duration by subscribing to the [`test:after:run`](https://on.cypress.io/catalog-of-events#Cypress-Events) event.
 
 ```js
 Cypress.on('test:after:run', (attributes) => {
--- examples/testing-dom__csv-table/cypress/integration/spec.js
+++ examples/testing-dom__csv-table/cypress/integration/spec.js
@@ -12,14 +12,14 @@ describe('CSV', () => {
 
   before(() => {
     cy.readFile('records.csv')
-    .then(neatCSV) // converts text into list of objects
-    .then((data) => {
-      table = data
-    })
-    .then(console.table) // convenient method for printing list of objects in DevTools console
+      .then(neatCSV) // converts text into list of objects
+      .then((data) => {
+        table = data
+      })
+      .then(console.table) // convenient method for printing list of objects in DevTools console
   })
 
-  it('checks table using DOM aliases (slow)', function () {
+  it('checks table using DOM aliases (slow)', () => {
     // convert list of objects into list of lists
     // rawData will be like
     // [ [Joe, Smith, student], [Mary, Sue, driver], ...]
@@ -28,25 +28,25 @@ describe('CSV', () => {
     cy.visit('index.html')
 
     cy.get('table tbody tr')
-    // make sure the table has loaded
-    .should('have.length.of', rawData.length - 1)
-    .as('cyRows')
+      // make sure the table has loaded
+      .should('have.length.of', rawData.length - 1)
+      .as('cyRows')
 
     for (let row = 0; row < rawData.length; row++) {
       cy.get('@cyRows')
-      .eq(row)
-      .find('td')
-      .as('cyRowColumns')
+        .eq(row)
+        .find('td')
+        .as('cyRowColumns')
 
       for (let column = 0; column < rawData[row].length; column++) {
         cy.get('@cyRowColumns')
-        .eq(column)
-        .should('contain', rawData[row][column])
+          .eq(column)
+          .should('contain', rawData[row][column])
       }
     }
   })
 
-  it('has table rows for each row of CSV file 🏃‍♂', function () {
+  it('has table rows for each row of CSV file 🏃‍♂', () => {
     cy.visit('index.html')
     cy.get('table tbody tr').should(($rows) => {
       // go through each row and confirm it shows the right information from CSV
@@ -55,21 +55,21 @@ describe('CSV', () => {
         const $cells = $row.children
 
         expect($cells[0])
-        .to.have.property('innerText')
-        .equal(record['First name'])
+          .to.have.property('innerText')
+          .equal(record['First name'])
 
         expect($cells[1])
-        .to.have.property('innerText')
-        .equal(record['Last name'])
+          .to.have.property('innerText')
+          .equal(record['Last name'])
 
         expect($cells[2])
-        .to.have.property('innerText')
-        .equal(record['Occupation'])
+          .to.have.property('innerText')
+          .equal(record['Occupation'])
       })
     })
   })
 
-  it('checks entire table row 🏇', function () {
+  it('checks entire table row 🏇', () => {
     // convert list of objects into list of lists
     // rawData will be like
     // [ [Joe, Smith, student], [Mary, Sue, driver], ...]
@@ -87,7 +87,7 @@ describe('CSV', () => {
     })
   })
 
-  it('checks entire object of values from DOM 🚀', function () {
+  it('checks entire object of values from DOM 🚀', () => {
     // convert list of objects into list of lists and then flatten
     // rawValues will be a single array
     // [Joe, Smith, student, Mary, Sue, driver, ...]
