3bfa35a9e9efedc0c3b1b98f033f21bd645f2caf
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-27 08:55:18 -0400

    add hooks spec example

---
+++ examples/unit-testing__react/cypress/component/counter-spec.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react'
+import { mount } from 'cypress-react-unit-test'
+import Counter from '../../counter.jsx'
+
+describe('Counter using hooks', () => {
+  it('works', () => {
+    mount(<Counter />)
+    cy.contains('You clicked 0 times')
+    cy.contains('Click me')
+    .click()
+    .click()
+    .click()
+
+    cy.contains('You clicked 3 times')
+  })
+})
