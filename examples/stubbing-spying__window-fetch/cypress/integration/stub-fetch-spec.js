40395ab54faa0590c1cde5a5a0c4631e5d6f6bf4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-06-25 07:56:44 -0400

    Update stubbing fetch recipe (#509)

--- examples/stubbing-spying__window-fetch/cypress/integration/stub-fetch-spec.js
+++ examples/stubbing-spying__window-fetch/cypress/integration/stub-fetch-spec.js
@@ -1,14 +1,39 @@
 /// <reference types="Cypress" />
+// @ts-check
 
 describe('stubbing', function () {
+  it('directly stubs window.fetch to test loading indicator', () => {
+    // stub the "fetch(/favorite-fruits)" call from the app
+    cy.visit('/', {
+      onBeforeLoad (win) {
+        cy.stub(win, 'fetch').withArgs('/favorite-fruits')
+        .resolves(
+          // use Bluebird promise bundled with Cypress
+          // to resolve after 2000ms
+          Cypress.Promise.resolve({
+            ok: true,
+            json: () => ['Pineapple 🍍'],
+          }).delay(2000)
+        )
+      },
+    })
+
+    // at first, the app is showing the loading indicator
+    cy.get('.loader').should('be.visible')
+    // once the promise is resolved, the loading indicator goes away
+    cy.get('.loader').should('not.exist')
+    cy.contains('li', 'Pineapple 🍍')
+  })
+
   // A big advantage of controlling the response is we can test
   // how our app handles a slow response, which normally might be
   // difficult against a fast development server
   it('shows loader while fetching fruits', function () {
+    // stub the XHR request from the app
     cy.server()
     cy.route({
       url: '/favorite-fruits',
-      reponse: [],
+      response: [],
       delay: 1000,
     })
 
@@ -17,6 +42,7 @@ describe('stubbing', function () {
 
     // once the network call finishes, the loader goes away
     cy.get('.loader').should('not.exist')
+    cy.contains('.favorite-fruits', 'No favorites')
   })
 
   it('can spy on network calls from the second page', () => {
