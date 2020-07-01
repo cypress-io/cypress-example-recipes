03fabd626f8f776433cc6eff3fa39e93b61ef1cd
KHeo <sainthkh@naver.com>
2020-07-01 10:16:36 +0900

    Add tests.

--- examples/fundamentals__typescript/cypress/integration/tests.spec.ts
+++ examples/fundamentals__typescript/cypress/integration/tests.spec.ts
@@ -1,6 +1,18 @@
-/// <reference types="Cypress" />
+/* global window */
+/// <reference path="../types.d.ts" />
 
-it('test custom command', () => {
-  cy.visit('https://cypress.io')
-  cy.clickLink('get started')
+// @ts-expect-error
+window.add(2, 3)
+
+describe('tests', () => {
+  it('test custom command', () => {
+    cy.visit('https://cypress.io')
+    cy.clickLink('get started')
+  })
+
+  it('test extending AUTWindow', () => {
+    cy.window().then((win) => {
+      return win.add(2, 3)
+    })
+  })
 })
--- examples/fundamentals__typescript/cypress/types.d.ts
+++ examples/fundamentals__typescript/cypress/types.d.ts
@@ -1,5 +1,11 @@
+/// <reference types="cypress" />
+
 declare namespace Cypress {
   interface Chainable<Subject=any> {
     clickLink(label: string | number | RegExp): void
+  }
+
+  interface ApplicationWindow {
+    add(a: number, b: number): number
   }
 }
