b89a685cb80de3d2214d5e61a0df3057336ecda4
KHeo <sainthkh@naver.com>
2020-07-01 12:19:15 +0900

    Test types.

--- examples/fundamentals__typescript/cypress/types.d.ts
+++ examples/fundamentals__typescript/cypress/types.d.ts
@@ -1,4 +1,15 @@
-/// <reference types="cypress" />
+// reference code is written like below to avoid the clash in mocha types.
+// in most of the cases, simple <reference types="cypress" /> will do.
+/// <reference path="../../../node_modules/cypress/types/cy-blob-util.d.ts" />
+/// <reference path="../../../node_modules/cypress/types/cy-bluebird.d.ts" />
+/// <reference path="../../../node_modules/cypress/types/cy-moment.d.ts" />
+/// <reference path="../../../node_modules/cypress/types/cy-minimatch.d.ts" />
+/// <reference path="../../../node_modules/cypress/types/lodash/index.d.ts" />
+/// <reference path="../../../node_modules/cypress/types/sinon/index.d.ts" />
+/// <reference path="../../../node_modules/cypress/types/jquery/index.d.ts" />
+/// <reference path="../../../node_modules/cypress/types/cypress.d.ts" />
+/// <reference path="../../../node_modules/cypress/types/cypress-type-helpers.d.ts" />
+/// <reference path="../../../node_modules/cypress/types/cypress-global-vars.d.ts" />
 
 declare namespace Cypress {
   interface Chainable<Subject=any> {
