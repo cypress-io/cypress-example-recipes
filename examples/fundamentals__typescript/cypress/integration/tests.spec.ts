595597abedd26867bc0eba0003d6b0b497b40783
KHeo <sainthkh@naver.com>
2020-07-03 16:16:47 +0900

    Make test clearer.

--- examples/fundamentals__typescript/cypress/integration/tests.spec.ts
+++ examples/fundamentals__typescript/cypress/integration/tests.spec.ts
@@ -1,11 +1,6 @@
 /* global window */
 /// <reference path="../types.d.ts" />
 
-before(() => {
-  // @ts-expect-error
-  window.add = (a, b) => a + b
-})
-
 describe('tests', () => {
   it('test custom command', () => {
     cy.visit('cypress/fixtures/test.html')
@@ -13,9 +8,14 @@ describe('tests', () => {
   })
 
   it('test extending AUTWindow', () => {
-    cy.window().then((win) => {
-      win.add = (a, b) => a + b
+    // Test Runner window object doesn't have add() function.
+    // So, it should fail the type check.
+    // @ts-expect-error
+    window.add = (a, b) => a + b
 
+    cy.window().then((win) => {
+      // AUT add() is defined in the fixture, test.html.
+      // So, it should pass the type check.
       return win.add(2, 3)
     })
   })
