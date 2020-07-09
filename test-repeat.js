d8ea758e1ac06e7400f92496faecb83b71d21a2c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-07-09 11:30:24 -0400

    use small utility script to run Cypress

---
+++ test-repeat.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-console */
+// a small utility script to run Cypress N times
+const arg = require('arg')
+const cypress = require('cypress')
+
+const args = arg({
+  '-n': Number,
+  '--group': String,
+})
+
+const runOptions = {}
+
+if (process.env.CYPRESS_RECORD_KEY) {
+  runOptions.record = true
+  runOptions.group = args['--group']
+}
+
+const onTestResults = (testResults) => {
+  if (testResults.failures) {
+    console.error(testResults.message)
+    process.exit(testResults.failures)
+  }
+
+  process.exit(testResults.totalFailed)
+}
+
+const onError = (err) => {
+  console.error(err)
+  process.exit(1)
+}
+
+cypress.run(runOptions).then(onTestResults, onError)
