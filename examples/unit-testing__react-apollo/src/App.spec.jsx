db2a27d94d5d65ee36c173fd1d37be048c004427
Chris Breiding <chrisbreiding@gmail.com>
2020-07-20 15:30:21 -0400

    skip the right test

--- examples/unit-testing__react-apollo/src/App.spec.jsx
+++ examples/unit-testing__react-apollo/src/App.spec.jsx
@@ -4,13 +4,6 @@ import { mount } from 'cypress-react-unit-test'
 import ApolloClient from 'apollo-boost'
 
 describe('App', () => {
-  it('loads real thing', () => {
-    mount(<App />)
-    // it might take a while to load real data from remote server
-    cy.get('[data-cy=book]', { timeout: 20000 })
-    .should('have.length.gte', 1)
-  })
-
   beforeEach(() => {
     cy.fixture('books')
     .then(JSON.stringify)
@@ -19,7 +12,14 @@ describe('App', () => {
 
   // FIXME: this tests fails due to a cross-origin error from codesandbox.io
   // https://github.com/cypress-io/cypress-example-recipes/issues/534
-  it.skip('can mock window fetch', function () {
+  it.skip('loads real thing', () => {
+    mount(<App />)
+    // it might take a while to load real data from remote server
+    cy.get('[data-cy=book]', { timeout: 20000 })
+    .should('have.length.gte', 1)
+  })
+
+  it('can mock window fetch', function () {
     cy.stub(window, 'fetch').withArgs('https://test/')
     .resolves({
       text: cy.stub().resolves(this.booksText).as('text'),
