780e7e3860bd1b991cfcb0adbfc578ab9c830e69
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-08-07 12:43:46 -0400

    comments

--- examples/fundamentals__timeout/README.md
+++ examples/fundamentals__timeout/README.md
@@ -1,3 +1,13 @@
 # Test timeout
 
+The example shows how to stop a test if it takes too long to execute, similar to [Mocha's timeout](https://mochajs.org/#test-level) option. See [cypress/integration/spec.js](cypress/integration/spec.js)
+
+```js
+// this test fails after two seconds due to timeout
+it('does not finish long tests', () => {
+  testTimeout(seconds(2))
+  cy.wait(seconds(10))
+})
+```
+
 ![Test is too long](images/test-is-too-long.png)
--- examples/fundamentals__timeout/cypress/integration/spec.js
+++ examples/fundamentals__timeout/cypress/integration/spec.js
@@ -3,7 +3,16 @@
 
 const seconds = (n) => n * 1000
 
+/**
+ * Stops the current Cypress test if it takes longer than the provided timeout
+ * @param {number} ms Test timeout in milliseconds
+ * @example
+ *  // stop and fail the test if it runs for longer than 10 seconds
+ *  testTimeout(10 * 1000)
+ */
 function testTimeout (ms) {
+  // get the current test reference using
+  // the cy.state() magic method
   const currentTest = cy.state('runnable')
   const startedAt = +new Date()
 
@@ -34,11 +43,14 @@ function testTimeout (ms) {
 }
 
 describe('Test timeout', () => {
+  // this test runs fine, because it finishes
+  // before the ten second limit expires
   it('allows short tests', () => {
     testTimeout(seconds(10))
     cy.wait(seconds(5))
   })
 
+  // this test fails after two seconds due to timeout
   // NOTE: enable to see test timeout in action
   it.skip('does not finish long tests', () => {
     testTimeout(seconds(2))
