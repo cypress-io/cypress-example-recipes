be8bf3702494f92314a32c3ae3388e067e61aa28
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-09-09 09:50:33 -0400

    try our path join

--- examples/fundamentals__fixtures/cypress/integration/require-fixtures-spec.js
+++ examples/fundamentals__fixtures/cypress/integration/require-fixtures-spec.js
@@ -4,7 +4,19 @@
 // which is "loaded" once
 const city = require('../fixtures/city.json')
 const country = require('../fixtures/country.json')
-const path = require('path')
+
+// bundled "os" module does not have the right EOL
+// thus we need to set it based on the platform ourself
+const EOL = Cypress.platform === 'win32' ? '\\' : '/'
+
+/**
+ * Joins parts of the file path using the EOL
+ * @example
+ *  join('cypress', 'integration')
+ *    // "cypress/integration" on non-Windows
+ *    // "cypress\integration" on Windows
+*/
+const join = (...paths) => paths.join(EOL)
 
 describe('requires fixtures', () => {
   it('has city', () => {
@@ -22,14 +34,13 @@ describe('requires fixtures', () => {
       expect(__dirname).to.be.a('string')
       // on Windows OS the directory is "cypress\integration"
       // on other operating systems it is "cypress/integration"
-      // use path.join to be cross-platform
-      expect(__dirname).to.equal(path.join('cypress', 'integration'))
+      expect(__dirname).to.equal(join('cypress', 'integration'))
     })
 
     it('has __filename', () => {
       expect(__filename).to.be.a('string')
       expect(__filename).to.equal(
-        path.join('cypress', 'integration', 'require-fixtures-spec.js')
+        join('cypress', 'integration', 'require-fixtures-spec.js')
       )
     })
   })
