452ea37b193403b259315097b2f4bf96c60c88ce
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-10-15 15:10:50 -0400

    add example redirecting static image

--- examples/stubbing-spying__route2/cypress/integration/image-spec.js
+++ examples/stubbing-spying__route2/cypress/integration/image-spec.js
@@ -22,4 +22,23 @@ describe('route2', () => {
 
     cy.visit('/pics.html')
   })
+
+  // skipping because sometimes crashes
+  // NOTE: https://github.com/cypress-io/cypress/issues/8858
+  it.skip('redirects static image', () => {
+    // instead of serving an image from a fixture
+    // we can redirect the request for the image
+    // to another route
+    cy.route2({
+      method: 'GET',
+      url: '/images/tiger.jpg',
+    }, (req) => {
+      // /__root/* is a special URL that can serve a file from the
+      // project's root. Normally this is used during bundling
+      // but here we are using it to serve another image
+      req.redirect('/__root/cypress/fixtures/roo.jpg')
+    })
+
+    cy.visit('/pics.html')
+  })
 })
