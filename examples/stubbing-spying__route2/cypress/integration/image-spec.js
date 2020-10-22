888d9831c96a331f5cf860fe57bf4a3e721c0f32
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-10-22 10:49:00 -0400

    update route2 example with binary image stub

--- examples/stubbing-spying__route2/cypress/integration/image-spec.js
+++ examples/stubbing-spying__route2/cypress/integration/image-spec.js
@@ -5,12 +5,11 @@ describe('route2', () => {
     cy.route2('/images').as('image')
     cy.visit('/pics.html')
     // how to check if the /image route was called once?
+    // @see https://github.com/cypress-io/cypress/issues/8934
     // cy.wait('@image')
   })
 
-  // https://github.com/cypress-io/cypress/issues/8623
-  // NOTE: seems to cause an infinite loop
-  it.skip('stubs a static image', () => {
+  it('stubs a static image', () => {
     // 🐅 -> kenguru
     cy.route2('/images', {
       fixture: 'roo.jpg',
@@ -21,6 +20,22 @@ describe('route2', () => {
     }).as('image')
 
     cy.visit('/pics.html')
+    // we DO see the roo image, but again, just like the test above
+    // cannot wait for it using cy.wait
+  })
+
+  it('stubs a static image using fixture', () => {
+    // 🐅 -> kenguru
+    cy.route2('/images', { fixture: 'roo.jpg' })
+    cy.visit('/pics.html')
+    // we DO see the roo image, but again, just like the test above
+    // cannot wait for it using cy.wait
+
+    // confirm the fixture has loaded by looking at its dimensions
+    // use "closeTo" assertion because sometimes browsers renders
+    // images with subpixel accuracy
+    cy.get('img').invoke('width').should('closeTo', 300, 1)
+    cy.get('img').invoke('height').should('closeTo', 450, 1)
   })
 
   // skipping because sometimes crashes
