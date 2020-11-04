8bdcdcad76a841fb2644a9f46c0d076c0986ee2e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-11-04 16:37:54 -0500

    make the offline spec work with devtools closed (#582)

--- examples/stubbing-spying__route2/cypress/integration/offline-spec.js
+++ examples/stubbing-spying__route2/cypress/integration/offline-spec.js
@@ -1,108 +1,173 @@
 /// <reference types="cypress" />
 
-/* eslint-env browser */
+/* global window */
 
-describe('route2', () => {
-  // since we are using Chrome debugger protocol API
-  // we should only run these tests when NOT in Firefox browser
-  context('app', { browser: '!firefox' }, () => {
-    // the application is making request to this url
-    const url = 'https://jsonplaceholder.cypress.io/users'
+// use window.navigator.onLine property to determine
+// if the browser is offline or online
+// https://caniuse.com/online-status
+const assertOnline = () => {
+  return cy.wrap(window).its('navigator.onLine').should('be.true')
+}
 
-    // https://caniuse.com/online-status
-    const assertOnline = () => {
-      return cy.wrap(window).its('navigator.onLine').should('be.true')
-    }
-    const assertOffline = () => {
-      return cy.wrap(window).its('navigator.onLine').should('be.false')
-    }
+const assertOffline = () => {
+  return cy.wrap(window).its('navigator.onLine').should('be.false')
+}
 
-    const goOffline = () => {
-      cy.log('**offline**')
-      .then(() => {
-        return Cypress.automation('remote:debugger:protocol',
-          {
-            command: 'Network.emulateNetworkConditions',
-            params: {
-              offline: true,
-              latency: -1,
-              downloadThroughput: -1,
-              uploadThroughput: -1,
-            },
-          })
+const goOffline = () => {
+  cy.log('**go offline**')
+  .then(() => {
+    return Cypress.automation('remote:debugger:protocol',
+      {
+        command: 'Network.enable',
       })
-    }
-
-    const goOnline = () => {
-      // disable offline mode, otherwise we will break our tests :)
-      cy.log('**online**')
-      .then(() => {
-        // https://chromedevtools.github.io/devtools-protocol/1-3/Network/#method-emulateNetworkConditions
-        return Cypress.automation('remote:debugger:protocol',
-          {
-            command: 'Network.emulateNetworkConditions',
-            params: {
-              offline: false,
-              latency: -1,
-              downloadThroughput: -1,
-              uploadThroughput: -1,
-            },
-          })
+  })
+  .then(() => {
+    return Cypress.automation('remote:debugger:protocol',
+      {
+        command: 'Network.emulateNetworkConditions',
+        params: {
+          offline: true,
+          latency: -1,
+          downloadThroughput: -1,
+          uploadThroughput: -1,
+        },
       })
-    }
+  })
+}
 
-    // make sure we get back online, even if a test fails
-    // otherwise the Cypress can lose the browser connection
-    beforeEach(goOnline)
-    afterEach(goOnline)
+const goOnline = () => {
+  // disable offline mode, otherwise we will break our tests :)
+  cy.log('**go online**')
+  .then(() => {
+    // https://chromedevtools.github.io/devtools-protocol/1-3/Network/#method-emulateNetworkConditions
+    return Cypress.automation('remote:debugger:protocol',
+      {
+        command: 'Network.emulateNetworkConditions',
+        params: {
+          offline: false,
+          latency: -1,
+          downloadThroughput: -1,
+          uploadThroughput: -1,
+        },
+      })
+  })
+  .then(() => {
+    return Cypress.automation('remote:debugger:protocol',
+      {
+        command: 'Network.disable',
+      })
+  })
+}
 
-    it('shows network status', () => {
-      cy.visit('/')
-      cy.contains('#network-status', 'online')
-      .wait(1000) // for demo purpose
+// since we are using Chrome debugger protocol API
+// we should only run these tests when NOT in Firefox browser
+describe('offline mode', { browser: '!firefox' }, () => {
+  // the application is making request to this url
+  const url = 'https://jsonplaceholder.cypress.io/users'
 
-      goOffline()
-      cy.contains('#network-status', 'offline')
-      .wait(1000) // for demo purpose
+  // make sure we get back online, even if a test fails
+  // otherwise the Cypress can lose the browser connection
+  beforeEach(goOnline)
+  afterEach(goOnline)
+
+  it('shows network status', () => {
+    cy.visit('/offline.html')
+    cy.contains('#network-status', 'online')
+    .wait(1000) // for demo purpose
+
+    goOffline()
+    cy.contains('#network-status', 'offline')
+    .wait(1000) // for demo purpose
+  })
+
+  it('shows error if we stub the network call', () => {
+    assertOnline()
+    cy.visit('/offline.html')
+    cy.route2(url, { forceNetworkError: true }).as('users')
+    cy.get('#load-users').click()
+    cy.contains('#users', 'Problem fetching users Failed to fetch')
+
+    // cannot wait for the route2 that forces network error
+    // https://github.com/cypress-io/cypress/issues/9062
+    // cy.wait('@users', { timeout: 1000 }) // the network call happens
+  })
+
+  it('shows error trying to fetch users in offline mode', () => {
+    cy.visit('/offline.html')
+    assertOnline()
+
+    // since this call returns a promise, must tell Cypress to wait
+    // for it to be resolved
+    goOffline()
+    assertOffline()
+
+    cy.get('#load-users').click()
+    cy.contains('#users', 'Problem fetching users Failed to fetch')
+
+    // now let's go back online and fetch the users
+    goOnline()
+    cy.get('#load-users').click()
+    cy.get('.user').should('have.length', 3)
+  })
+
+  it('makes fetch request when offline', () => {
+    cy.visit('/offline.html')
+
+    goOffline()
+    assertOffline()
+
+    // let's spy on the "fetch" method the app calls
+    cy.window().then((w) => cy.spy(w, 'fetch').withArgs(`${url}?_limit=3`).as('fetchUsers'))
+
+    cy.get('#load-users').click()
+    cy.get('@fetchUsers').should('have.been.calledOnce')
+
+    // now let's go back online and fetch the users
+    goOnline()
+    cy.get('#load-users').click()
+    cy.get('.user').should('have.length', 3)
+    cy.get('@fetchUsers').should('have.been.calledTwice')
+  })
+
+  it('does not reach the outside network when offline', () => {
+    cy.visit('/offline.html')
+
+    // before we go offline we have to set up network intercepts
+    // since they need to be communicated outside the browser
+    // and lets keep track the number of network calls made
+    let callCount = 0
+
+    cy.route2(url, () => {
+      callCount += 1
+    }).as('users')
+
+    goOffline()
+    assertOffline()
+
+    cy.get('#load-users').click()
+    cy.contains('#users', 'Problem fetching users Failed to fetch')
+
+    // the cy.route2 network call does NOT happen
+    // because the browser does not fire it
+    // and thus our network proxy does not see it
+    cy.then(() => {
+      expect(callCount, 'no network calls made').to.equal(0)
     })
 
-    it('shows error if we stub the network call', () => {
-      assertOnline()
-      cy.visit('/')
-      cy.route2(url, { forceNetworkError: true }).as('users')
-      cy.get('#load-users').click()
-      cy.contains('#users', 'Problem fetching users Failed to fetch')
-
-      // cannot wait for the route2 that forces network error
-      // https://github.com/cypress-io/cypress/issues/9062
-      // cy.wait('@users', { timeout: 1000 }) // the network call happens
+    // now let's go back online and fetch the users
+    goOnline()
+    cy.get('#load-users').click()
+    // we can retry the assertion to know when the network call has happened
+    // using .should callback function with an assertion inside
+    .should(() => {
+      expect(callCount, 'single network call').to.equal(1)
     })
 
-    // NOTE: https://github.com/cypress-io/cypress/issues/9063
-    it.skip('shows error trying to fetch users in offline mode', () => {
-      assertOnline()
+    cy.wait('@users')
+    .its('response.body')
+    .then(JSON.parse)
+    .should('have.length', 3)
 
-      cy.route2(url).as('users')
-
-      cy.visit('/')
-
-      assertOnline()
-
-      // since this call returns a promise, must tell Cypress to wait
-      // for it to be resolved
-      goOffline()
-
-      assertOffline()
-
-      // let's spy on the "fetch" method the app calls
-      cy.window().then((w) => cy.spy(w, 'fetch').withArgs(`${url}?_limit=3`).as('fetchUsers'))
-      cy.get('#load-users').click()
-
-      cy.get('@fetchUsers').should('have.been.calledOnce')
-
-      // the cy.route2 network call does NOT happen
-      // because the browser does not fire it
-      // and thus our network proxy does not see it
-    })
+    cy.get('.user').should('have.length', 3)
   })
 })
