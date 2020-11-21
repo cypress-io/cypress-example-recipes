b8344fe4d92afef23dbcee512c09ad0a6fafa286
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-11-21 16:31:21 -0500

    add ajax call duration assertion with test retries

---
+++ examples/server-communication__xhr-assertions/cypress/integration/duration-spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+describe('XHR', () => {
+  it('is faster than 300ms', { retries: 3 }, () => {
+    cy.visit('index.html')
+
+    // before the request goes out we need to set up spying
+    // see https://on.cypress.io/network-requests
+    cy.server()
+    cy.route('POST', '/posts').as('post')
+
+    cy.get('#load').click()
+    cy.wait('@post').its('duration').should('be.lessThan', 300)
+  })
+})
