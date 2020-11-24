6bfb726c61f85321bacb21a3906a3c336ed96523
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-11-24 13:22:17 -0500

    add example showing how to intercept requests with query object

--- examples/stubbing-spying__intercept/app.js
+++ examples/stubbing-spying__intercept/app.js
@@ -36,26 +36,31 @@ function getFavoriteFruits () {
 getFavoriteFruits()
 setInterval(getFavoriteFruits, 30000)
 
-document.getElementById('load-users').addEventListener('click', () => {
-  console.log('loading users')
-  document.querySelector('#users').innerText = ''
+const loadUsers = (nUsers = 3) => {
+  return () => {
+    console.log('loading %d users', nUsers)
+    document.querySelector('#users').innerText = ''
 
-  fetch('https://jsonplaceholder.cypress.io/users?_limit=3')
-  .then((r) => r.json())
-  .then((users) => {
-    console.table(users)
+    fetch(`https://jsonplaceholder.cypress.io/users?_limit=${nUsers}`)
+    .then((r) => r.json())
+    .then((users) => {
+      console.table(users)
 
-    const usersHtml = users.map((user) => {
-      return `<li class="user">${user.id} - ${user.email}</li>`
-    }).join('\n')
+      const usersHtml = users.map((user) => {
+        return `<li class="user">${user.id} - ${user.email}</li>`
+      }).join('\n')
 
-    document.querySelector('#users').innerHTML = usersHtml
-  })
-  .catch((e) => {
-    console.error('problem fetching users', e)
-    document.querySelector('#users').innerText = `Problem fetching users ${e.message}`
-  })
-})
+      document.querySelector('#users').innerHTML = usersHtml
+    })
+    .catch((e) => {
+      console.error('problem fetching users', e)
+      document.querySelector('#users').innerText = `Problem fetching users ${e.message}`
+    })
+  }
+}
+
+document.getElementById('load-users').addEventListener('click', loadUsers(3))
+document.getElementById('load-five-users').addEventListener('click', loadUsers(5))
 
 const updateNetworkStatus = () => {
   const el = document.getElementById('network-status')
