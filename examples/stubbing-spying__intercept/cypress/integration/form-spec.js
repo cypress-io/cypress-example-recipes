23feab15d07bb1bb0b17908e4239f6ed1dfb6e0d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-01-08 11:56:21 -0500

    form spec

--- examples/stubbing-spying__intercept/cypress/integration/form-spec.js
+++ examples/stubbing-spying__intercept/cypress/integration/form-spec.js
@@ -16,6 +16,34 @@ describe('intercept', () => {
     cy.wait('@submitForm').its('request.url').then((url) => {
       // the url has form fields in its query
       console.log('form submit url', url)
+      // let's parse the URL and extract the search parameters
+      const parsed = new URL(url)
+      // convert URLSearchParams into a plain object
+      const searchParams = Object.fromEntries(parsed.searchParams)
+
+      // compare the form submission to the expected values
+      expect(searchParams).to.deep.equal({
+        fname: 'Joe',
+        lname: 'Smith',
+      })
     })
+
+    // equivalent fluent function chain
+    // due to JavaScript "new" keyword, have to create a helper
+    // https://glebbahmutov.com/blog/work-around-the-keyword-new-in-javascript/
+    const invokeConstructor = (constructor) => (arg) => new constructor(arg)
+
+    cy.get('@submitForm')
+    .its('request.url')
+    .then(invokeConstructor(URL))
+    .its('searchParams')
+    .then(Object.fromEntries)
+    .should('deep.equal', {
+      fname: 'Joe',
+      lname: 'Smith',
+    })
+
+    // make sure our redirect has worked
+    cy.location('pathname').should('equal', '/form')
   })
 })
