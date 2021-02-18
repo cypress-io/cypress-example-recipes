094bf835e9b8044476d770773724bd545eb48ce3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-18 09:51:05 -0500

    check if the intercept has the custom added header

--- examples/stubbing-spying__intercept/cypress/integration/header-spec.js
+++ examples/stubbing-spying__intercept/cypress/integration/header-spec.js
@@ -22,4 +22,20 @@ describe('intercept', { viewportHeight: 300 }, () => {
     .should('contain', 'x-custom-header')
     .and('contain', 'added by cy.intercept')
   })
+
+  it('adds request header', () => {
+    cy.visit('/headers')
+    cy.intercept('/req-headers', (req) => {
+      // note: this header will NOT be shown in the browser's
+      // Network tab, as the request has already left the browser
+      // when this header is added
+      req.headers['x-custom-headers'] = 'added by cy.intercept'
+    }).as('headers')
+
+    cy.get('#get-headers').click()
+    // but the custom request header added by the intercept
+    // will be in object we can get by waiting on the intercept
+    cy.wait('@headers').its('request.headers')
+    .should('have.property', 'x-custom-headers', 'added by cy.intercept')
+  })
 })
