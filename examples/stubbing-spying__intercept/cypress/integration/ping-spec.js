fd667d329fc183bc7cab584f184f5d3be16e852a
Gleb Bahmutov <bahmutov@yahoo.com>
2021-02-21 21:35:49 -0500

    add anchor ping example test (#645)

---
+++ examples/stubbing-spying__intercept/cypress/integration/ping-spec.js
@@ -0,0 +1,18 @@
+/// <reference types="Cypress" />
+// <a ping="..."> syntax is behind a flag in Firefox
+describe('intercept', { browser: '!firefox' }, () => {
+  it('stubs anchor ping', () => {
+    cy.visit('/')
+    cy.intercept({
+      method: 'POST',
+      pathname: '/track',
+    }, {}).as('ping')
+
+    cy.get('a[ping]').click()
+    cy.location('pathname').should('equal', '/page2.html')
+    cy.wait('@ping').its('request.headers').should('deep.include', {
+      'ping-from': 'http://localhost:7080/',
+      'ping-to': 'http://localhost:7080/page2.html',
+    })
+  })
+})
