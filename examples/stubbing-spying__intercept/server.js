55c7e00c50001ced24957735ae589f9c07decee2
Gleb Bahmutov <bahmutov@yahoo.com>
2021-02-22 14:21:45 -0500

    add example spying on JSONP request (#647)

--- examples/stubbing-spying__intercept/server.js
+++ examples/stubbing-spying__intercept/server.js
@@ -35,12 +35,30 @@ app.get('/fruits', (req, res) => {
   res.sendFile(`${__dirname}/fruits.html`)
 })
 
-app.get('/headers', (req, res) => {
-  res.sendFile(`${__dirname}/headers.html`)
+app.get('/fruits-jsonp', (req, res) => {
+  res.sendFile(`${__dirname}/fruits-jsonp.html`)
+})
+
+app.get('/favorite-fruits-jsonp', (req, res) => {
+  // we expect the query to have the function name "fruitsCallback"
+  // we need to call when the result gets back into the browser
+  console.log(req.query)
+
+  // return random 4 fruits for JSONP requests
+  const selectedFruits = _.sampleSize(fruits, 4)
+  const selectedFruitsJS = JSON.stringify(selectedFruits)
+
+  res.header('Content-Type', 'application/javascript')
+  res.header('Charset', 'utf-8')
+  res.send(`${req.query.fruitsCallback}(${selectedFruitsJS})`)
 })
 
 app.get('/favorite-fruits', (req, res) => {
   res.json(_.sampleSize(fruits, 5))
+})
+
+app.get('/headers', (req, res) => {
+  res.sendFile(`${__dirname}/headers.html`)
 })
 
 app.get('/logout', (req, res) => {
