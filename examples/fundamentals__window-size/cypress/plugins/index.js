97a0d25fb2a30ed62484d26951d9e1ec29f6a54b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-25 10:14:11 -0500

    print browser size

--- examples/fundamentals__window-size/cypress/plugins/index.js
+++ examples/fundamentals__window-size/cypress/plugins/index.js
@@ -17,10 +17,14 @@ module.exports = function (on, config) {
   // https://glebbahmutov.com/blog/cypress-tips-and-tricks/#produce-high-quality-video-recording
   // https://on.cypress.io/browser-launch-api
   on('before:browser:launch', (browser = {}, launchOptions) => {
+    console.log('launching browser %s is headless? %s', browser.name, browser.isHeadless)
+
     // the browser width and height we want to get
     // our screenshots and videos will be of that resolution
     const width = 1920
     const height = 1080
+
+    console.log('setting the browser window size to %d x %d', width, height)
 
     if (browser.name === 'chrome' && browser.isHeadless) {
       launchOptions.args.push(`--window-size=${width},${height}`)
