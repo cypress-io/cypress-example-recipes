96537ab4100a599dcdb46a3dee3410694b404007
mikedlr <siblemitcom.mddlr@spamgourmet.com>
2021-03-11 20:27:56 +0000

    correct wrong comment - password is invalid.

--- examples/logging-in__jwt/cypress/integration/using-ui-spec.js
+++ examples/logging-in__jwt/cypress/integration/using-ui-spec.js
@@ -50,7 +50,7 @@ describe('logs in', () => {
     cy.visit('/')
     cy.location('pathname').should('equal', '/login')
 
-    // enter valid username and password
+    // enter username with invalid password
     cy.get('[name=username]').type('username')
     cy.get('[name=password]').type('wrong-password')
     cy.contains('button', 'Login').click()
