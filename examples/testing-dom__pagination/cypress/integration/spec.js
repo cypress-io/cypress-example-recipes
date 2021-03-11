3d002b125428cbc81fe6fdac223bc6204838a897
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-03-11 11:05:55 -0500

    show how to iterate yourself

--- examples/testing-dom__pagination/cypress/integration/spec.js
+++ examples/testing-dom__pagination/cypress/integration/spec.js
@@ -3,8 +3,38 @@
 import { recurse } from 'cypress-recurse'
 
 describe('pagination', () => {
+  // iterate recursively until the "Next" link is disabled
+  // then assert we are on the last page
+  it('goes to the last page', () => {
+    cy.visit('public/index.html')
+
+    const visitTextPageIfPossible = () => {
+      cy.get('.paginationjs-next').then(($next) => {
+        if ($next.hasClass('disabled')) {
+          // we are done - we are on the last page
+          return
+        }
+
+        cy.wait(500) // just for clarity
+        cy.get('.paginationjs-next').click()
+        visitTextPageIfPossible()
+      })
+    }
+
+    visitTextPageIfPossible()
+
+    cy.log('**on the last page**')
+    cy.contains('li', 11).should('be.visible')
+    cy.contains('.paginationjs-nav', '3 / 3').should('be.visible')
+    // if you want to confirm the "next" list is disabled
+    cy.get('.paginationjs-next').should('have.class', 'disabled')
+  })
+
   it('goes to the last page (using recurse)', () => {
     cy.visit('public/index.html')
+
+    // cypress-recurse has built-in options for
+    // iterating until the predicate returns true
     recurse(
       () => cy.get('.paginationjs-next'),
       (next) => next.hasClass('disabled'),
@@ -16,6 +46,7 @@ describe('pagination', () => {
       }
     )
 
+    cy.log('**on the last page**')
     cy.contains('li', 11).should('be.visible')
     cy.contains('.paginationjs-nav', '3 / 3').should('be.visible')
     // if you want to confirm the "next" list is disabled
