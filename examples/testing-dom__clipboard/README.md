eb3df37724a20a319ff193c8776c9e8403cf3fc6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-05-24 22:15:32 -0400

    add example with clipboard copy and paste

--- examples/testing-dom__clipboard/README.md
+++ examples/testing-dom__clipboard/README.md
@@ -1,3 +1,10 @@
 # Clipboard
+> Copy / paste text example
 
-The `@github/clipboard-copy-element` element comes from [github/github-elements](https://github.com/github/github-elements)
+The widget to copy text is `@github/clipboard-copy-element` custom element that comes from [github/github-elements](https://github.com/github/github-elements)
+
+![Copy / paste test](./images/copy-paste.gif)
+
+See the [cypress/integration/spec.js](./cypress/integration/spec.js) file.
+
+The page [index.html](./index.html) shows the copy button on "mouseover" event. When the text is copied to the clipboard, it shows a [tiny toast](https://github.com/bahmutov/tiny-toast) popup.
--- examples/testing-dom__clipboard/cypress/integration/spec.js
+++ examples/testing-dom__clipboard/cypress/integration/spec.js
@@ -1,9 +1,22 @@
 /// <reference types="cypress" />
 
-/* eslint-disable no-console */
-
 describe('Clipboard', () => {
   it('copies text to clipboard', () => {
     cy.visit('index.html')
+    cy.get('code').trigger('mouseover')
+    cy.get('[aria-label="Copy"]').click()
+
+    cy.contains('.tinyToast', 'Copied!').should('be.visible')
+    // the toast then goes away in less than 2 seconds
+    cy.get('.tinyToast', { timeout: 2000 }).should('not.exist')
+
+    // let's check the copied text
+    cy.window().its('navigator.clipboard')
+    .invoke('readText')
+    .should('equal', 'npm install -D cypress')
+    .then((text) => {
+      // paste the text from the clipboard into the text area
+      cy.get('#paste-here').click().invoke('val', text)
+    })
   })
 })
