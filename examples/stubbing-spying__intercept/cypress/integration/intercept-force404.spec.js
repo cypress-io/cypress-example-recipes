cfc96845b70b7e5654bfbac3cad45aa4b8ddbc59
Mike <14354013+mccataldo@users.noreply.github.com>
2021-06-25 12:02:00 -0600

    add example test

--- examples/stubbing-spying__intercept/cypress/integration/intercept-force404.spec.js
+++ examples/stubbing-spying__intercept/cypress/integration/intercept-force404.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 
-describe("intercept", () => {
+describe('intercept', () => {
   beforeEach(() => {
     // this solution assumes:
     // - you're only dealing with XHR requests
@@ -11,19 +11,30 @@ describe("intercept", () => {
     cy.intercept(
       {
         headers: {
-          accept: "application/json",
+          accept: 'application/json',
         },
       },
       {
         statusCode: 404,
       }
-    );
+    )
 
     // DO NOT DO THIS:
     // cy.intercept('*', { statusCode: 404 })
     // as it will break all network requests
-  });
-  it("test", () => {
-    cy.visit("/");
-  });
-});
+  })
+
+  it('test', () => {
+    cy.intercept({
+      method: 'POST',
+      pathname: '/users',
+    }).as('postUser')
+
+    cy.visit('/')
+    // triggers POST /users request and gets real response:
+    cy.get('#post-user').click()
+    cy.wait('@postUser')
+    // triggers PUT /users/1 request but gets stubbed 404 response:
+    cy.get('#put-user').click()
+  })
+})
