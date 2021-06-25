6eaebf9def2e6d236fa78f1da33e6824423f515c
Mike <14354013+mccataldo@users.noreply.github.com>
2021-06-25 12:15:05 -0600

    update example

--- examples/stubbing-spying__intercept/cypress/integration/glob-matching-intercept-url.spec.js
+++ examples/stubbing-spying__intercept/cypress/integration/glob-matching-intercept-url.spec.js
@@ -7,7 +7,8 @@ describe('intercept', () => {
   // will match https://example.com/api/roles
   // will match https://example.com/api/roles?country=US
 
-  cy.intercept('/api/roles?*utm_source=+(My%App|https*)')
-  // will match https://example.com/api/roles?utm_medium=Nav&utm_campaign=Docs&utm_source=My%App
+  cy.intercept('/api/roles?*utm_source=+(MyApp|https*)')
+  // will match https://example.com/api/roles?utm_medium=Nav&utm_campaign=Docs&utm_source=MyApp
   // will match https://example.com/api/roles?utm_medium=Nav&utm_campaign=Docs&utm_source=https://....
+  // will not match https://example.com/api/roles?utm_medium=Nav&utm_campaign=Docs&utm_source=marketing
 })
