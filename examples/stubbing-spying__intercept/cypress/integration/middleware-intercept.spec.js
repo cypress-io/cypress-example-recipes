551978044e58fe57b9e41ac49e5d292bbe64a61c
Mike <14354013+mccataldo@users.noreply.github.com>
2021-06-25 12:49:12 -0600

    fix eslint errors

--- examples/stubbing-spying__intercept/cypress/integration/middleware-intercept.spec.js
+++ examples/stubbing-spying__intercept/cypress/integration/middleware-intercept.spec.js
@@ -1,19 +1,19 @@
-describe("intercept", () => {
+describe('intercept', () => {
   beforeEach(() => {
     // Put these in a global `beforeEach` for top-level middleware handlers that
     // affect all requests. Setting `middleware: true` will
     // cause these handlers to always be called first.
 
     // Set an auth token on all requests:
-    cy.intercept("/**", { middleware: true }, (req) => {
-      req.headers["authorization"] = `token ${token}`;
-    });
+    cy.intercept('/**', { middleware: true }, (req) => {
+      req.headers['authorization'] = 'Bearer AbCdEf123456'
+    })
 
     // Delay API responses to simulate real-world conditions:
-    cy.intercept({ url: "/**", middleware: true }, (req) => {
-      req.on("response", (res) => {
-        res.setDelay(1000);
-      });
-    });
-  });
-});
+    cy.intercept({ url: '/**', middleware: true }, (req) => {
+      req.on('response', (res) => {
+        res.setDelay(1000)
+      })
+    })
+  })
+})
