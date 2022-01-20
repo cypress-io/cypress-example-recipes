97067f0f84ebec249a19d881888613d367073dcc
Paul Jaffre <jaffrepaul@gmail.com>
2022-01-20 12:34:48 -0500

    config test

--- examples/blogs__a11y/cypress.config.js
+++ examples/blogs__a11y/cypress.config.js
@@ -1,3 +1,10 @@
-const { defineConfig } = require("cypress")
+const { defineConfig } = require('cypress')
 
-module.exports = defineConfig({})
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents (on, config) {
+      // e2e testing node events setup code
+      console.log('setupNodeEvents config:', config)
+    },
+  },
+})
