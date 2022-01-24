06bf34edc6a9002464fb06182ea6a5e494444c79
ElevateBart <ledouxb@gmail.com>
2022-01-24 10:22:35 -0600

    Test with real 10.0.0 binary (manually again without only)
    ```json
    {
      "platform": "linux",
      "arch": "x64",
      "env": {
        "CYPRESS_INSTALL_BINARY": "https://cdn.cypress.io/beta/binary/10.0.0/linux-x64/circle-10.0-release-8f32960ef803f539f065d41f01fff33bfe33ed5d/cypress.zip"
      },
      "packages": "https://cdn.cypress.io/beta/npm/10.0.0/circle-10.0-release-8f32960ef803f539f065d41f01fff33bfe33ed5d/cypress.tgz",
      "branch": "10.0.0",
      "commit": "8f32960ef803f539f065d41f01fff33bfe33ed5d",
      "status": {
        "owner": "cypress-io",
        "repo": "cypress",
        "sha": "8f32960ef803f539f065d41f01fff33bfe33ed5d",
        "platform": "linux",
        "arch": "x64",
        "context": "[linux-x64] cypress-****-ci-environments"
      }
    }
    ```

--- examples/preprocessors__typescript-webpack/cypress/e2e/spec.cy.ts
+++ examples/preprocessors__typescript-webpack/cypress/e2e/spec.cy.ts
@@ -32,12 +32,12 @@ describe('TypeScript', () => {
   })
 
   // enable once we release updated TypeScript definitions
-  it.only('has Cypress object type definition', () => {
-    console.log({Cypress});
-    console.log('hello');
+  // it.skip('has Cypress object type definition', () => {
+  //   console.log({ Cypress })
+  //   console.log('hello')
 
-    expect(Cypress.version).to.be.a('string')
-  })
+  //   expect(Cypress.version).to.be.a('string')
+  // })
 
   // wrong code on purpose to type check our definitions
   // it('can visit website', () => {
