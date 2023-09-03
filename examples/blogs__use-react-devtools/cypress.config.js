0d45c8be5ba88f20ef19b2d17b87747c46abbafe
Bill Glesias <bglesias@gmail.com>
2023-09-03 13:28:14 -0400

    chore: fix react extension loading

--- examples/blogs__use-react-devtools/cypress.config.js
+++ examples/blogs__use-react-devtools/cypress.config.js
@@ -12,7 +12,7 @@ module.exports = defineConfig({
         // only load React DevTools extension when opening Chrome in interactive mode
         if (browser.family === 'chromium') {
           // we could also restrict the extension to only load when browser.isHeaded is true
-          const extensionFolder = path.resolve(__dirname, '..', '..', '4.2.1_0')
+          const extensionFolder = path.resolve(__dirname, '4.2.1_0')
 
           console.log('adding React DevTools extension from', extensionFolder)
           launchOptions.args.push(extensionFolder)
