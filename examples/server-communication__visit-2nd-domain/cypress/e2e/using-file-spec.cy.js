f20c4c8a242a1a21b053043c7a1fed87a479c301
Matthew Schile <mschile@cypress.io>
2024-11-06 08:51:58 -0700

    fixing tests

--- examples/server-communication__visit-2nd-domain/cypress/e2e/using-file-spec.cy.js
+++ examples/server-communication__visit-2nd-domain/cypress/e2e/using-file-spec.cy.js
@@ -2,23 +2,11 @@
 describe('Two domains using file', () => {
   const filename = 'test-data.json'
 
-  Cypress.on('uncaught:exception', (err) => {
-    // cypress.io has a few React exceptions related to state hydration,
-    // but these exceptions do not impact this test
-    // This is also true with Cannot read properties of null (reading 'addEventListener') which has to due with the osano library
-    // that is on www.cypress.io
-    if (err.message.includes('Minified React error') || err.message.includes(`Cannot read properties of null (reading 'addEventListener')`)) {
-      return false
-    }
-
-    return true
-  })
-
   it('visits 1st domain', () => {
-    cy.visit('https://www.cypress.io/')
+    cy.visit('https://example.cypress.io/')
     // there are several GitHub links on the page, make sure
     // to use the selector that returns a single item
-    cy.get('[href="https://github.com/cypress-io/cypress"]').first()
+    cy.get('[href="https://github.com/cypress-io/cypress-example-kitchensink"]').first()
     .invoke('attr', 'href')
     .then((url) => {
       expect(url).to.be.a('string')
