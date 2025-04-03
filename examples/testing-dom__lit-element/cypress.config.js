ba0228bb8c7370a7594666599193883398e9fff8
AtofStryker <bglesias@gmail.com>
2025-04-03 12:32:05 -0400

    additionally update lit CDN

--- examples/testing-dom__lit-element/cypress.config.js
+++ examples/testing-dom__lit-element/cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   fixturesFolder: false,
-  experimentalShadowDomSupport: true,
   e2e: {
     supportFile: false,
   },
